477629a28eec444130a811de7402933d77b0e770
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2008-04-10 17:23:18 +0000

    Added two routines.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wspr@690 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ wspr1.f90
@@ -0,0 +1,40 @@
+subroutine wspr1
+
+#ifdef CVF
+  use dfmt
+  integer Thread0
+  external wspr2
+#endif
+
+  include 'acom1.f90'
+
+#ifdef CVF
+!  Priority classes (for processes):
+!     IDLE_PRIORITY_CLASS               64
+!     NORMAL_PRIORITY_CLASS             32
+!     HIGH_PRIORITY_CLASS              128
+
+!  Priority definitions (for threads):
+!     THREAD_PRIORITY_IDLE             -15
+!     THREAD_PRIORITY_LOWEST            -2
+!     THREAD_PRIORITY_BELOW_NORMAL      -1
+!     THREAD_PRIORITY_NORMAL             0
+!     THREAD_PRIORITY_ABOVE_NORMAL       1
+!     THREAD_PRIORITY_HIGHEST            2
+!     THREAD_PRIORITY_TIME_CRITICAL     15
+
+  nrxdone=0
+  m0=SetPriorityClass(GetCurrentProcess(),NORMAL_PRIORITY_CLASS)
+! Start a thread for acquiring audio data
+  Thread0=CreateThread(0,0,wspr2,0,CREATE_SUSPENDED,id0)
+  m1=SetThreadPriority(Thread0,THREAD_PRIORITY_NORMAL)
+  m2=ResumeThread(Thread0)
+#else
+! Start a thread for acquiring audio data
+!  ierr=start_threads(ndevin,ndevout,y1,y2,nmax,iwrite,iwave,nwave,        &
+!       11025,NSPB,TRPeriod,TxOK,ndebug,Transmitting,                      &
+!       Tsec,ngo,nmode,tbuf,ibuf,ndsec,PttPort,devin_name,devout_name)
+#endif
+
+  return
+end subroutine wspr1
