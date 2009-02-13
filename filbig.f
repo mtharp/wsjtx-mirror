47f33b0f6f9894df7ca6b2554cc77251735cdd6b
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2009-02-13 15:18:33 +0000

    Removed half of FFTs from filbig.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/map65iq@1058 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- filbig.f
+++ filbig.f
@@ -8,14 +8,14 @@ C  f_stop = 750 Hz.
       parameter (NFFT1=5376000,NFFT2=77175)
       integer*2 id(4,nmax)                       !Input data
       complex c4a(NFFT2),c4b(NFFT2)              !Output data
-      complex ca(NFFT1),cb(NFFT1)                !FFTs of input
+      complex ca(NFFT1)                          !FFT of input
       real*8 df
 C Impulse response of filter (one side)
       real halfpulse(8)
       complex cfilt(NFFT2)
                        !Filter (complex; imag = 0)
       real rfilt(NFFT2)                          !Filter (real)
-      integer plan1,plan2,plan3,plan4,plan5
+      integer plan1,plan3,plan5
       logical first
       include 'fftw3.f'
       equivalence (rfilt,cfilt)
@@ -31,11 +31,7 @@ C Impulse response of filter (one side)
 C  Plan the FFTs just once
          call sfftw_plan_dft_1d_(plan1,NFFT1,ca,ca,
      +        FFTW_BACKWARD,npatience)
-         call sfftw_plan_dft_1d_(plan2,NFFT1,cb,cb,
-     +        FFTW_BACKWARD,npatience)
          call sfftw_plan_dft_1d_(plan3,NFFT2,c4a,c4a,
-     +        FFTW_FORWARD,npatience)
-         call sfftw_plan_dft_1d_(plan4,NFFT2,c4b,c4b,
      +        FFTW_FORWARD,npatience)
          call sfftw_plan_dft_1d_(plan5,NFFT2,cfilt,cfilt,
      +        FFTW_BACKWARD,npatience)
@@ -68,17 +64,14 @@ C  If we just have a new f0, continue with the existing ca and cb.
          nz=min(nmax,NFFT1)
          do i=1,nz
             ca(i)=cmplx(float(int(id(1,i))),float(int(id(2,i))))
-            cb(i)=cmplx(float(int(id(3,i))),float(int(id(4,i))))
          enddo
 
          if(nmax.lt.NFFT1) then
             do i=nmax+1,NFFT1
                ca(i)=0.
-               cb(i)=0.
             enddo
          endif
          call sfftw_execute_(plan1)
-         call sfftw_execute_(plan2)
          newdat=0
       endif
 
@@ -90,25 +83,26 @@ C      i0 is the bin number in ca and cb closest to f0.
       do i=1,nh                                !Copy data into c4a and c4b,
          j=i0+i-1                              !and apply the filter function
          c4a(i)=rfilt(i)*ca(j)
-         c4b(i)=rfilt(i)*cb(j)
       enddo
       do i=nh+1,NFFT2
          j=i0+i-1-NFFT2
          if(j.lt.1) j=j+NFFT1                  !NFFT1 was NFFT2
          c4a(i)=rfilt(i)*ca(j)
-         c4b(i)=rfilt(i)*cb(j)
       enddo
 
 C  Do the short reverse transform, to go back to time domain.
       call sfftw_execute_(plan3)
-      call sfftw_execute_(plan4)
+
+C### temp:
+      do i=1,NFFT2
+         c4b(i)=c4a(i)
+      enddo
+
       n4=min(nmax/64,NFFT2)
       go to 999
 
  900  call sfftw_destroy_plan_(plan1)
-      call sfftw_destroy_plan_(plan2)
       call sfftw_destroy_plan_(plan3)
-      call sfftw_destroy_plan_(plan4)
       call sfftw_destroy_plan_(plan5)
 
  999  return
