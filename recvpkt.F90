8826f1934a153691cc1ab0290f503e6320640c64
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2009-02-17 03:08:31 +0000

    Temporary changes only ...
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/map65iq@1074 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- recvpkt.F90
+++ recvpkt.F90
@@ -38,7 +38,9 @@ subroutine recvpkt(iarg)
   nseclr=msec/1000                  !Time according to Linrad
 
 !  fcenter=center_freq
-  fcenter=center_freq + 133.4d0                      !### Temporary ###
+!  fcenter=center_freq + 133.4d0                      !### Temporary ###
+!  fcenter=center_freq + 134.d0                      !### Temporary ###
+  fcenter=center_freq + 137.1d0                      !### Temporary ###
 
 ! Reset buffer pointers at start of minute.
   ns=mod(nsec,60)
@@ -95,8 +97,8 @@ subroutine recvpkt(iarg)
         k2=k2-NSMAX
         n=2
      endif
-     id(1,k2,2)=id(1,k2,n)/8                  !### Temporary? ###
-     id(2,k2,2)=id(2,k2,n)/8                  !### Temporary? ###
+!     id(1,k2,2)=id(1,k2,n)/8                  !### Temporary? ###
+!     id(2,k2,2)=id(2,k2,n)/8                  !### Temporary? ###
      x1=id(1,k2,n)
      x2=id(2,k2,n)
      sq=sq + x1*x1 + x2*x2
@@ -116,7 +118,9 @@ subroutine recvpkt(iarg)
         if(ns.ge.nt1 .and. ndone1.eq.0 .and. synced) then
            nutc=mutc
 !           fcenter=center_freq                  !### Temporary ###
-           fcenter=center_freq + 133.4d0             !### Temporary ###
+!           fcenter=center_freq + 133.4d0             !### Temporary ###
+!           fcenter=center_freq + 134.d0             !### Temporary ###
+           fcenter=center_freq + 137.1d0             !### Temporary ###
            kbuf=kb
            kk=k
            ndiskdat=0
