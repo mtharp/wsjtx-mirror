2588a4b56ddd7f44dc241273d4a1873d7723abf8
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2009-04-30 18:48:47 +0000

    t441 now without any FFTs.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjt8@1185 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- t441.f90
+++ t441.f90
@@ -4,13 +4,13 @@ program t441
 
   parameter (NSTEP=3)                       !Step size for lag (samples)
   parameter (NP=1200/NSTEP)                 !Period of full message (lags)
-  parameter (MAXFFT=4096)                   !FFT length
   character*28 tmsg
   real ps(128)                              !Spectrum computed in WSJT
   real dat(9283)                            !Raw data, 11025 S/s
+  real s1(9283)
+  real s2(9283)
   integer itone(3*28)                       !Tones of test message
   complex cz(3*28*25)                       !Complex LO for test message
-  complex c(MAXFFT)                         !Mixed signal
   complex csum
   data twopi/6.2831853/
 
@@ -32,55 +32,55 @@ program t441
   read(88) jjz,ps,f0,(dat(j),j=1,jjz)       !Read raw data saved by WSJT
   df1=11025.0/256.0                         !df for the ps() spectrum
 
-  xdf=-152.
   df0=441.0                                 !Tone spacing
   dt=1.0/11025.0                            !Sample interval
-  phi=0.                                    !Initialize phase
-  j0=999
-  do i=1,nsamp                      !Generate conjugate of message waveform
-     j=(i-1)/25 + 1
-     if(j.ne.j0) then
-        freq=882.0 + xdf + itone(j)*df0
-        dphi=twopi*freq*dt
-        j0=j
-     endif
-     phi=phi+dphi
-     cz(i)=0.001*cmplx(cos(phi),-sin(phi))
-  enddo
 
-! Find best match to test message over all lags and all frequency offsets
-  nfft=512
-  nh=nfft/2
-  df=11025.0/nfft
-  k=0
+  df=11025.0/(10*nsamp)
+  ndf=221.0/df
   sbest=0.
-  do lag=0,9000,NSTEP
-     k=k+1
-     c=0.
-     do i=1,nsamp
-        c(i)=cz(i) * dat(i+lag)
+  print*,nsamp,df,ndf
+
+  do idf=-ndf,ndf
+     xdf=idf*df
+     phi=0.                                    !Initialize phase
+     j0=999
+     do i=1,nsamp                      !Generate conjugate of message waveform
+        j=(i-1)/25 + 1
+        if(j.ne.j0) then
+           freq=882.0 + xdf + itone(j)*df0
+           dphi=twopi*freq*dt
+           j0=j
+        endif
+        phi=phi+dphi
+        cz(i)=0.001*cmplx(cos(phi),-sin(phi))
      enddo
-     call four2a(c,nfft,1,-1,1)
-     smax=0.
-     do i=1,nh+1
-        s=(real(c(i))**2 + aimag(c(i))**2)
-        if(s.gt.smax) then
-           smax=s
-           ipk=i
+
+     ! Find best match to test message over all lags and all frequency offsets
+     k=0
+     do lag=0,9283-nsamp,NSTEP
+        k=k+1
+        csum=0.
+        do i=1,nsamp
+           csum=csum + cz(i)*dat(i+lag)
+        enddo
+        s=real(csum)**2 + aimag(csum)**2
+        s1(k)=s
+        if(s.gt.sbest) then
+           sbest=s
+           idfpk=idf
+           lagpk=lag
         endif
      enddo
-     write(13,3002) lag,smax,ipk
-3002 format(i6,f10.0,i8)
-     if(smax.gt.sbest) then
-        sbest=smax
-        npk=ipk
-        kpk=k
-     endif
+     if(idfpk.eq.idf) s2(1:k)=s1(1:k)
   enddo
   kz=k
-  fpk=(npk-1)*df
-!  xdf=fpk-foffset
-  write(*,1030) fpk,kpk,sbest
+  xdfpk=idfpk*df
+  write(*,1030) xdfpk,lagpk,sbest
 1030 format('Fpk:',f7.0,'   Lagpk:',i4,'   Sbest:',f8.0)
+
+  do k=1,kz
+     write(13,1040) k,s2(k)
+1040 format(i5,f10.0)
+  enddo
   
 999 end program t441
