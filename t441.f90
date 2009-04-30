a3b91a542bc24f54aa1dc35741de457ea934746b
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2009-04-30 20:36:09 +0000

    Doing FFT of s2() to determine period.  (Next, do harmonic summing.)
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjt8@1188 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- t441.f90
+++ t441.f90
@@ -2,31 +2,35 @@ program t441
 
 ! Run tests on the "TNX QSO TNX QSO ..." ping in W8WN sample file.
 
-  parameter (NSTEP=3)                       !Step size for lag (samples)
-  parameter (NP=1200/NSTEP)                 !Period of full message (lags)
+  parameter (NPTS=9283)
   character*28 tmsg
+  character*12 arg
   real ps(128)                              !Spectrum computed in WSJT
-  real dat(9283)                            !Raw data, 11025 S/s
-  real s1(9283)
-  real s2(9283)
+  real dat(NPTS)                            !Raw data, 11025 S/s
+  real s1(NPTS)
+  real s2(NPTS)
+  real p(28*3*25)                           !Folded s2
   integer itone(3*28)                       !Tones of test message
   complex cz(3*28*25)                       !Complex LO for test message
   complex csum
+  complex cs2(0:2048)
+  equivalence(s2,cs2)
   data twopi/6.2831853/
 
   nargs=iargc()
-  if(nargs.ne.1) then
-     print*,'Usage: t441 "Test message"'
+  if(nargs.ne.2) then
+     print*,'Usage: t441 "Test message" nstep'
      go to 999
   endif
   call getarg(1,tmsg)
+  call getarg(2,arg)
+  read(arg,*) nstep
   call gen441(tmsg,nmsg,itone)
   nsym=3*nmsg
   nsamp=25*nsym
 
-  write(*,1000) nmsg,nsamp,8,8*75
-1000 format('Test msg:',i3,' ch,'i4,' samples.'/               &
-            'Full msg:',i3,' ch,'i4,' samples.')
+  write(*,1000) nmsg,nsamp
+1000 format('Test msg:',i3,' chars,'i4,' samples.')
 
   open(88,file='dat.88',form='unformatted',status='old')
   read(88) jjz,ps,f0,(dat(j),j=1,jjz)       !Read raw data saved by WSJT
@@ -38,7 +42,6 @@ program t441
   df=11025.0/(10*nsamp)
   ndf=221.0/df
   sbest=0.
-  print*,nsamp,df,ndf
 
   do idf=-ndf,ndf
      xdf=idf*df
@@ -57,7 +60,7 @@ program t441
 
      ! Find best match to test message over all lags and all frequency offsets
      k=0
-     do lag=0,9283-nsamp,NSTEP
+     do lag=0,NPTS-nsamp,nstep
         k=k+1
         csum=0.
         do i=1,nsamp
@@ -75,6 +78,8 @@ program t441
   enddo
   kz=k
   xdfpk=idfpk*df
+  write(*,1020) nmsg,2*ndf+1,kz
+1020 format('Nmsg:',i3,'   Nf:',i4,'   Nt:',i5)
   write(*,1030) xdfpk,lagpk,sbest
 1030 format('Fpk:',f7.0,'   Lagpk:',i4,'   Sbest:',f8.0)
 
@@ -82,5 +87,48 @@ program t441
      write(13,1040) k,s2(k)
 1040 format(i5,f10.0)
   enddo
-  
+
+  pbest=0.
+  do np=2,28
+     jz=np*75/nstep
+     nsum=kz/jz
+     kz2=nsum*jz
+     p(1:jz)=0.
+     do k=1,kz2
+        j=mod(k-1,jz)+1
+        p(j)=p(j)+s2(k)
+     enddo
+     lu=20+np
+     fac=sqrt(1.0/nsum)
+     pmax=0.
+     do j=1,jz
+        p(j)=fac*p(j)
+        if(p(j).gt.pmax) then
+           pmax=p(j)
+           if(p(j).gt.pbest) then
+              pbest=p(j)
+              npbest=np
+           endif
+        endif
+        write(lu,1050) j,p(j)
+1050    format(i5,f10.0)
+     enddo
+     write(14,1060) np,pmax
+1060 format(i5,f10.0)
+  enddo
+  print*,npbest,pbest,kz
+
+  nfft=4096
+  nh=nfft/2
+  s2(kz+1:nfft)=0.
+  s2(1:kz)=1.e-4*s2(1:kz)
+
+  call four2a(s2,nfft,1,-1,0)
+  do i=1,nh
+     ss2=real(cs2(i))**2 + aimag(cs2(i))**2
+     period=float(nfft)/i
+     write(15,1070) i,ss2,period
+1070 format(i5,f10.3,f10.3)
+  enddo
+
 999 end program t441
