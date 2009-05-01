67e78e0ede0878dfdf6591c72ca5c7c75bf207a3
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2009-05-01 15:34:51 +0000

    Use ACF in t441.  Seems quite robust in determining length of message.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjt8@1189 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- t441.f90
+++ t441.f90
@@ -9,28 +9,27 @@ program t441
   real dat(NPTS)                            !Raw data, 11025 S/s
   real s1(NPTS)
   real s2(NPTS)
+  real acf(500)
   real p(28*3*25)                           !Folded s2
   integer itone(3*28)                       !Tones of test message
   complex cz(3*28*25)                       !Complex LO for test message
   complex csum
-  complex cs2(0:2048)
-  equivalence(s2,cs2)
   data twopi/6.2831853/
 
   nargs=iargc()
-  if(nargs.ne.2) then
-     print*,'Usage: t441 "Test message" nstep'
+  if(nargs.ne.3) then
+     print*,'Usage: t441 "Test message" tstep fdiv'
      go to 999
   endif
   call getarg(1,tmsg)
   call getarg(2,arg)
   read(arg,*) nstep
+  call getarg(3,arg)
+  read(arg,*) fdiv
+
   call gen441(tmsg,nmsg,itone)
   nsym=3*nmsg
   nsamp=25*nsym
-
-  write(*,1000) nmsg,nsamp
-1000 format('Test msg:',i3,' chars,'i4,' samples.')
 
   open(88,file='dat.88',form='unformatted',status='old')
   read(88) jjz,ps,f0,(dat(j),j=1,jjz)       !Read raw data saved by WSJT
@@ -39,13 +38,13 @@ program t441
   df0=441.0                                 !Tone spacing
   dt=1.0/11025.0                            !Sample interval
 
-  df=11025.0/(10*nsamp)
+  df=11025.0/(fdiv*nsamp)
   ndf=221.0/df
   sbest=0.
 
-  do idf=-ndf,ndf
+  do idf=-ndf,ndf                           !Loop over allowed range of DF
      xdf=idf*df
-     phi=0.                                    !Initialize phase
+     phi=0.                                 !Initialize phase
      j0=999
      do i=1,nsamp                      !Generate conjugate of message waveform
         j=(i-1)/25 + 1
@@ -58,77 +57,48 @@ program t441
         cz(i)=0.001*cmplx(cos(phi),-sin(phi))
      enddo
 
-     ! Find best match to test message over all lags and all frequency offsets
      k=0
-     do lag=0,NPTS-nsamp,nstep
+     do idt=0,NPTS-nsamp,nstep             !Loop over time offset DT
         k=k+1
         csum=0.
         do i=1,nsamp
-           csum=csum + cz(i)*dat(i+lag)
+           csum=csum + cz(i)*dat(i+idt)
         enddo
         s=real(csum)**2 + aimag(csum)**2
         s1(k)=s
         if(s.gt.sbest) then
            sbest=s
            idfpk=idf
-           lagpk=lag
+           idtpk=idt
         endif
      enddo
      if(idfpk.eq.idf) s2(1:k)=s1(1:k)
   enddo
   kz=k
   xdfpk=idfpk*df
-  write(*,1020) nmsg,2*ndf+1,kz
-1020 format('Nmsg:',i3,'   Nf:',i4,'   Nt:',i5)
-  write(*,1030) xdfpk,lagpk,sbest
-1030 format('Fpk:',f7.0,'   Lagpk:',i4,'   Sbest:',f8.0)
 
   do k=1,kz
      write(13,1040) k,s2(k)
 1040 format(i5,f10.0)
   enddo
 
-  pbest=0.
-  do np=2,28
-     jz=np*75/nstep
-     nsum=kz/jz
-     kz2=nsum*jz
-     p(1:jz)=0.
-     do k=1,kz2
-        j=mod(k-1,jz)+1
-        p(j)=p(j)+s2(k)
+  do lag=0,2500/nstep
+     sum=0.
+     do i=1,kz-lag
+        sum=sum + s2(i)*s2(i+lag)
      enddo
-     lu=20+np
-     fac=sqrt(1.0/nsum)
-     pmax=0.
-     do j=1,jz
-        p(j)=fac*p(j)
-        if(p(j).gt.pmax) then
-           pmax=p(j)
-           if(p(j).gt.pbest) then
-              pbest=p(j)
-              npbest=np
-           endif
-        endif
-        write(lu,1050) j,p(j)
-1050    format(i5,f10.0)
-     enddo
-     write(14,1060) np,pmax
-1060 format(i5,f10.0)
+     acf(lag)=1.e-3*sum/(kz-lag)
+     tp=lag*nstep/75.0
+     write(14,1050) lag,tp,acf(lag)
+1050 format(i5,f10.3,f13.3)
+     if(tp.gt.1.5 .and. acf(lag).gt.acfmax) then
+        acfmax=acf(lag)
+        ppk=tp
+     endif
   enddo
-  print*,npbest,pbest,kz
 
-  nfft=4096
-  nh=nfft/2
-  s2(kz+1:nfft)=0.
-  s2(1:kz)=1.e-4*s2(1:kz)
-
-  call four2a(s2,nfft,1,-1,0)
-  do i=1,nh
-     ss2=real(cs2(i))**2 + aimag(cs2(i))**2
-     period=float(nfft)/i
-     write(15,1070) i,ss2,period
-1070 format(i5,f10.3,f10.3)
-  enddo
+  write(*,1020) tmsg(1:8),nmsg,2*ndf+1,kz,nint(xdfpk),idtpk,nint(sbest),ppk
+1020 format(a8,'  Nmsg:',i3,'  Nf:',i4,'  Nt:',i5,'  DF:',i4,      &
+            '  DT:',i5,'  S:'i6,'  P:',f7.2)
 
 999 end program t441
