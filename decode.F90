71a2f373887268508e09b3fddce9155d0a72eb41
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2009-10-28 19:21:47 +0000

    If nsave=1, copy iwave into jwave before decoding; then save jwave.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wspr@1592 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- decode.F90
+++ decode.F90
@@ -6,15 +6,18 @@ subroutine decode
   use dfport
 #endif
   character*80 savefile
+  integer*2 jwave(114*12000)
 
   include 'acom1.f90'
 
   minsync=1
+  if(nsave.gt.0 .and. ndiskdat.eq.0) jwave=iwave
+
   call mept162(thisfile,f0,minsync,iwave,NMAX,nbfo,ierr)
   if(nsave.gt.0 .and. ndiskdat.eq.0 .and. ierr.eq.0) then
      savefile='save/'//thisfile
      npts=114*12000
-     call wfile5(iwave,npts,12000,savefile)
+     call wfile5(jwave,npts,12000,savefile)
   endif
   write(14,1100)
 1100 format('$EOF')
