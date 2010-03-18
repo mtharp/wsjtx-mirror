1d4e1047d794f151082cdb664307b253a00c4df6
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2010-03-18 21:01:26 +0000

    Add fmtave.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wspr@1847 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ fmtave.f90
@@ -0,0 +1,55 @@
+program fmtave
+
+  implicit real*8 (a-h,o-z)
+  character infile*80
+  character*8 cutc,cutc1
+
+  nargs=iargc()
+  if(nargs.ne.1) then
+     print*,'Usage: fmtave <infile>'
+     go to 999
+  endif
+  call getarg(1,infile)
+
+  open(10,file=infile,status='old')
+
+  nkhz0=0
+  sum=0.d0
+  sumsq=0.d0
+  n=0
+  do i=1,99999
+     read(10,*,end=10) cutc,nkHz,noffset,faudio,snr,yave,yrms
+     if(nkHz.ne.nkHz0 .and. i.ne.1) then
+        ave=sum/n
+        rms=0.d0
+        if(n.gt.1) then
+           rms=sqrt(abs(sumsq - sum*sum/n)/(n-1.d0))
+        endif
+        err=rms/sqrt(n-1.d0)
+        if(err.lt.0.1) err=0.1
+        fMHz=0.001d0*nkHz0
+        write(*,1010) fMHz,ave,rms,err,cutc1
+1010    format(f8.3,3f8.2,2x,a8)
+        sum=0.d0
+        sumsq=0.d0
+        n=0
+     endif
+     dial_error=faudio-noffset
+     sum=sum + dial_error
+     sumsq=sumsq + dial_error**2
+     n=n+1
+     if(n.eq.1) cutc1=cutc
+     nkHz0=nkHz
+  enddo
+
+10 ave=sum/n
+  rms=0.d0
+  if(n.gt.0) then
+     rms=sqrt((sumsq - sum*sum/n)/(n-1.d0))
+  endif
+  err=rms/sqrt(n-1.d0)
+  if(err.lt.0.1) err=0.1
+  fMHz=0.001d0*nkHz
+  write(*,1010) fMHz,ave,rms,err,cutc1
+
+999 end program fmtave
