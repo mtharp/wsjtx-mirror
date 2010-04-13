fc48b1c4bb0a0306dd22b05873510a6bb8268c06
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2010-04-13 18:35:44 +0000

    Temporarily add binary file of raw pings.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjt8a@1890 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- tms.f90
+++ tms.f90
@@ -23,6 +23,20 @@ program tms
   do iter=1,999
      read(10,end=999) iping,jz,(dat(i),i=1,jz)
      if(iping.eq.nping .or. nping.eq.0) then
+
+        k=0
+        nadd=200
+        do j=1,jz/nadd
+           sq=0.
+           do i=1,nadd
+              k=k+1
+              sq=sq + dat(k)**2
+           enddo
+           sdb=db(sq/nadd) - 46
+           write(13,1002) j/120.0,sdb
+1002       format(f8.3,f10.3)
+        enddo
+
         call syncms(dat,jz,NFreeze,MouseDF,DFTolerance,snrsync,   &
              dfx,lagbest,isbest,nerr,metric,decoded)
         nsnr=nint(db(snrsync)-2.0)
@@ -36,10 +50,9 @@ program tms
         if(nsnr.ge.9) nrpt=nrpt+1
         c1=' '
         if(nsnr.ge.2 .and. isbest.ne.0) c1='*'
-        cfile6='000000'
-        write(*,1010) cfile6,dtx,mswidth,nsnr,nrpt,ndf,isbest,c1,    &
+        write(*,1010) iping,dtx,mswidth,nsnr,nrpt,ndf,isbest,c1,    &
              decoded,nerr,metric
-1010    format(a6,f6.1,i5,i4,i4,i6,i3,a1,2x,a24,i7,i5)
+1010    format(i3,f6.1,i5,i4,i4,i6,i3,a1,2x,a24,i7,i5)
         if(nping.ne.0) go to 999
      endif
   enddo
