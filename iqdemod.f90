13072180d7c0c609709a164ef19866e2342398aa
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2010-09-13 02:26:34 +0000

    Now receiving OK in I-Q mode, with SoftRock RxTx.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wspr@2138 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- iqdemod.f90
+++ iqdemod.f90
@@ -5,14 +5,14 @@ subroutine iqdemod(kwave,jz)
 
   twopi=8.d0*atan(1.d0)
   dt=1.d0/48000.d0
-  f0=8700.d0
+  f0=8700.d0                 + 60
   dpha=twopi*f0*dt
   pha=0.d0
 
   npts=jz/2
   do i=1,npts
-     x=kwave(2*i-1)
-     y=kwave(2*i)
+     y=kwave(2*i-1)                             !Reversed?
+     x=kwave(2*i)
      pha=pha+dpha
      r=cmplx(x,y)*cmplx(cos(pha),-sin(pha))
      if(r.lt.-32767.0) r=-32767.0
