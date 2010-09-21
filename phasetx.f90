5c1cdef81a354323d4902e8a889355d297f95604
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2010-09-21 02:07:44 +0000

    add missing file
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wspr@2175 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ phasetx.f90
@@ -0,0 +1,27 @@
+subroutine phasetx(id2,npts,fac,ntxbal,ntxpha)
+
+  integer*2 id2(2,npts)
+  complex h,z
+
+  pha=0.01*ntxpha/57.2957795
+  xbal=1.0 + 0.001*ntxbal
+  if(xbal.gt.1.0) then
+     b1=1.0
+     b2=1.0/xbal
+  else
+     b1=xbal
+     b2=1.0
+  endif
+  do i=1,npts
+     x=id2(1,i)
+     y=id2(2,i)
+     phi=atan2(y,x)
+     xx=30000.0*cos(phi)
+     yy=30000.0*sin(phi+pha)
+     z=fac*cmplx(xx,yy)
+     id2(1,i)=b1*real(z)
+     id2(2,i)=b2*aimag(z)
+  enddo
+
+  return
+end subroutine phasetx
