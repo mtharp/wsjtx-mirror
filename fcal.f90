6d90a1568ba5ed4472e01270d2fc662d9812773f
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2010-10-29 16:51:54 +0000

    Further tweaks of the FMT package.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wspr@2245 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- fcal.f90
+++ fcal.f90
@@ -5,6 +5,8 @@ program fcal
   real*8 fd(NZ),deltaf(NZ),r(NZ)
   character infile*50
   character line*80
+  character cutc*8
+  character*6 callsign(NZ)
 
   nargs=iargc()
   if(nargs.ne.1) then
@@ -26,13 +28,16 @@ program fcal
      if(i0.le.0 .and. i1.le.0) then
         read(line,*,err=5) f,df
         ncal=1
+        callsign(i)='      '
      else
-        read(line,*,err=5) f,df,ncal
+        read(line,*,err=5) f,df,ncal,nn,rr,cutc,callsign(i)
      endif
-     i=i+1
-     fd(i)=f
-     deltaf(i)=df
-     r(i)=0.d0
+     if(ncal.eq.1) then
+        i=i+1
+        fd(i)=f
+        deltaf(i)=df
+        r(i)=0.d0
+     endif
 5    continue
   enddo
 
@@ -41,22 +46,22 @@ program fcal
   call fit(fd,deltaf,r,iz,a,b,sigmaa,sigmab,rms)
 
   write(*,1002) 
-1002 format('    Freq     DF     Meas Freq    Resid'/        &
-            '   (MHz)    (Hz)      (MHz)       (Hz)'/        &
-            '--------------------------------------')       
+1002 format('    Freq      DF     Meas Freq     Resid  Call'/        &
+            '   (MHz)     (Hz)      (MHz)        (Hz)'/        &
+            '------------------------------------------------')       
   do i=1,iz
      fm=fd(i) + 1.d-6*deltaf(i)
      calfac=1.d0 + 1.d-6*deltaf(i)/fd(i)
-     write(*,1010) fd(i),deltaf(i),fm,r(i)
-     write(13,1010) fd(i),deltaf(i),fm,r(i)
-1010 format(f8.3,f8.2,f14.9,f8.2)
+     write(*,1010) fd(i),deltaf(i),fm,r(i),callsign(i)
+     write(13,1010) fd(i),deltaf(i),fm,r(i),callsign(i)
+1010 format(f8.3,f9.3,f14.9,f9.3,2x,a6)
   enddo
   calfac=1.d0 + 1.d-6*b
   err=1.d-6*sigmab
 
   if(iz.ge.3) then
      write(*,1100) a,b,rms
-1100 format(/'A:',f8.2,' Hz    B:',f9.4,' ppm    StdDev:',f6.2,' Hz')
+1100 format(/'A:',f8.2,' Hz    B:',f9.4,' ppm    StdDev:',f7.3,' Hz')
   if(iz.gt.2) write(*,1110) sigmaa,sigmab
 1110 format('err:',f6.2,9x,f9.4,23x,f13.9)
   else
--- fmtave.f90
+++ fmtave.f90
@@ -3,10 +3,12 @@ program fmtave
   implicit real*8 (a-h,o-z)
   character infile*80
   character*8 cutc,cutc1
+  character*6 callsign,callsign0
 
   nargs=iargc()
   if(nargs.ne.1) then
-     print*,'Usage: fmtave <infile>'
+     print*,'Usage:   fmtave <infile>'
+     print*,'Example: fmtave fmt.all'
      go to 999
   endif
   call getarg(1,infile)
@@ -15,15 +17,15 @@ program fmtave
   open(12,file='fmtave.out',status='unknown')
 
   write(*,1000)
-1000 format('    Freq    DF    CAL   N     rms    UTC'/                &
+1000 format('    Freq    DF    CAL   N     rms    UTC     Call'/       &
             '   (kHz)   (Hz)    ?         (Hz)'/                       &
-            '-------------------------------------------')
+            '---------------------------------------------------')
   nkhz0=0
   sum=0.d0
   sumsq=0.d0
   n=0
   do i=1,99999
-     read(10,*,end=10) cutc,nkHz,ncal,noffset,faudio,snr,yave,yrms
+     read(10,*,end=10) cutc,nkHz,ncal,noffset,faudio,df,dblevel,snr,callsign
      if(nkHz.ne.nkHz0 .and. i.ne.1) then
         ave=sum/n
         rms=0.d0
@@ -31,9 +33,9 @@ program fmtave
            rms=sqrt(abs(sumsq - sum*sum/n)/(n-1.d0))
         endif
         fMHz=0.001d0*nkHz0
-        write(*,1010)  fMHz,ave,ncal,n,rms,cutc1
-        write(12,1010) fMHz,ave,ncal,n,rms,cutc1
-1010    format(2f8.3,i4,i5,f8.2,2x,a8)
+        write(*,1010)  fMHz,ave,ncal,n,rms,cutc1,callsign0
+        write(12,1010) fMHz,ave,ncal,n,rms,cutc1,callsign0
+1010    format(2f8.3,i4,i5,f8.2,2x,a8,2x,a6)
         sum=0.d0
         sumsq=0.d0
         n=0
@@ -44,6 +46,7 @@ program fmtave
      n=n+1
      if(n.eq.1) cutc1=cutc
      nkHz0=nkHz
+     callsign0=callsign
   enddo
 
 10 ave=sum/n
@@ -52,7 +55,7 @@ program fmtave
      rms=sqrt((sumsq - sum*sum/n)/(n-1.d0))
   endif
   fMHz=0.001d0*nkHz
-  write(*,1010)  fMHz,ave,ncal,n,rms,cutc1
-  write(12,1010) fMHz,ave,ncal,n,rms,cutc1
+  write(*,1010)  fMHz,ave,ncal,n,rms,cutc1,callsign0
+  write(12,1010) fMHz,ave,ncal,n,rms,cutc1,callsign0
 
 999 end program fmtave
