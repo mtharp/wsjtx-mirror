4f30fc872a3c860eac525ac94aa01dfe27d8ac81
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2013-01-24 21:31:00 +0000

    Oops!  Two more files added.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/trunk@2979 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ snr4.f90
@@ -0,0 +1,27 @@
+subroutine snr4(blue,sync,snr)
+
+  real blue(65)
+  integer ipk1(1)
+  equivalence (ipk,ipk1)
+
+  ipk1=maxloc(blue)
+  ns=0
+  s=0.
+  do i=1,65 
+     if(abs(i-ipk).gt.1) then
+        s=s+blue(i)
+        ns=ns+1
+     endif
+  enddo
+  base=s/ns
+  blue=blue-base
+  sq=0.
+  do i=1,65
+     if(abs(i-ipk).gt.1) sq=sq+blue(i)**2
+  enddo
+  rms=sqrt(sq/(ns-1))
+  snr=10.0*log10(blue(ipk)/rms) - 30.6
+  sync=snr+25.5
+
+  return
+end subroutine snr4
