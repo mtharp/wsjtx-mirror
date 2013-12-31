7622b26f9d5294b35e3c6fcc60c434401e1d68ee
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2013-12-31 13:52:06 +0000

    Should not have changed rh.bat.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wspr212@3616 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- WSPR.iss
+++ WSPR.iss
@@ -1,6 +1,6 @@
 [Setup]
 AppName=WSPR
-AppVerName=WSPR Version 2.12 r3615
+AppVerName=WSPR Version 2.12 r3616
 
 AppCopyright=Copyright (C) 2008-2014 by Joe Taylor, K1JT
 DefaultDirName={pf}\WSPR
--- rh.bat
+++ rh.bat
@@ -1,1 +1,2 @@
-"C:\Users\Joe\progs\reshack\ResHacker.exe" -addoverwrite wspr.exe,wspr.exe,wsjt.ico,ICONGROUP,MAINICON,0
+"C:\Program Files\ResHacker\reshack\ResHacker.exe" -addoverwrite wspr.exe,wspr.exe,wsjt.ico,ICONGROUP,MAINICON,0
+
