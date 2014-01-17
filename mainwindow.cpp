aa44f15e62dd7110b79e0d7098d7145a589113a6
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-01-17 19:56:11 +0000

    Display of yellow curve is working again.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx_w@3646 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1239,7 +1239,7 @@ void MainWindow::on_actionOpen_next_in_directory_triggered()   //Open Next
   int i,len;
   QFileInfo fi(m_path);
   QStringList list;
-  list= fi.dir().entryList().filter(".wav");
+  list= fi.dir().entryList().filter(".wav",Qt::CaseInsensitive);
   for (i = 0; i < list.size()-1; ++i) {
     if(i==list.size()-2) m_loopall=false;
     len=list.at(i).length();
