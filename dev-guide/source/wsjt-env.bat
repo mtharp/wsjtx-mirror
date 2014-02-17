8ef33d5f9851b88a154b76c503a37409a6f83c7f
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-02-17 09:26:41 +0000

    *Dev-Guide Re-Write*
    *Part 2 of 2 Commit*
    
    Only requires 2 steps form the user to build, open the WSJT Env and Run the
    build script. Users still need to Download & Install Development tools. Uses SVN
    to download any needed files.
    
    *wsjtx-dev.adoc*
    - most of the Windows CMake section was re-written
    - Linux Makefile section will be updated to the same method
    
    *Setup Script Added :: wsjtx-build-setup.bat*
    - No Copy & Paste needed now
    - creates all the directories
    - downloads the build and cmake scripts
    - allows for user defined install location c:\ d:\tools etc etc
    - final env directory is [path]\wsjt-env
    - tests directory structure on subsequent runs, adds needed dir's
    - uses svn export to pull the build and cmake file updates
    - does not check Build Tool Dir's as that is done when wsjt-env starts
    
    *wsjt-env.bat*
    - checks if all the required build tools are present, if not, displays error msg
    telling the user what is missing, then exits.
    - lists the tool versions so you can verify what is being used
    - displays the two build methods on the screen at sucessful startup
    
    *wsjtx-build-cmake.bat*
    - requires the wsjt-env terminal to ensure it's not going to build without all
    required packages
    - cannot be run from Double-Click or Right-Click >> Open, for the same reason
    - downloads, builds and copys all files needed to run WSJT-X.
    - builds Debug or Release version, default is Release
    - has some error handling for SVN aborts. Does a simple svn cleanup then exits
    
    TO-DO
    - More testing, especially on WinXP
    - Minor edits, and add error handling as appropriate.
    - grammer, text polishing, syntax tweaks
    
    :: @QX9770-SRV1/Win64 ==> Cygwin-Terminal
    
    
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/doc@3757 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- dev-guide/source/wsjt-env.bat
+++ dev-guide/source/wsjt-env.bat
@@ -2,27 +2,64 @@
 REM -- Custom CMD Window for WSJT-X Build using CMake
 REM -- Part of the WSJT Documentation project
 REM -- Change color so users know this is *not* a normal CMD Window
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
 COLOR 1B
 TITLE WSJT Environment
 CLS
-REM -- Be careful editing these variables, if wrong, the build will fail!
-SET BASED=C:\wsjt-env
-SET CMAKED=C:\wsjt-env\CMake\bin
-SET SVND=C:\wsjt-env\SlikSvn\bin
-SET QTD=C:\wsjt-env\Qt5\5.2.1\mingw48_32\bin
-SET TOOLSD=C:\wsjt-env\Qt5/Tools/mingw48_32\bin
+REM -- Be careful with variables, if incorrect, WSJT Env Terminal will fail.
+SET TARGET=%~dp0
+IF %TARGET:~-1%==\ SET TARGET=%TARGET:~0,-1%
+SET BASED=%TARGET%
+SET CMAKED=%BASED%\CMake\bin
+SET SVND=%BASED%\SlikSvn\bin
+SET QTD=%BASED%\Qt5\5.2.1\mingw48_32\bin
+SET TOOLSD=%BASED%\Qt5/Tools/mingw48_32\bin
 SET PATH=%BASED%;%CMAKED%;%SVND%;%QTD%;%TOOLSD%;%PATH%
 CD /D %BASED%
+
+REM -- CHECK REQUIRED TOOLS
+cmake --version >nul 2>null || SET APP=CMake && GOTO ERROR1
+qmake --version || SET APP=QMake && GOTO ERROR1
+svn --version >nul 2>null || SET APP=SVN && GOTO ERROR1
+mingw32-make --version >nul 2>null || SET APP=Mingw32-Make && GOTO ERROR1
+gfortran --version >nul 2>null || SET APP=Gfortran && GOTO ERROR1
+gcc --version >nul 2>null || SET APP=GCC && GOTO ERROR1
+g++ --version >nul 2>null || SET APP=G++ && GOTO ERROR1
+GOTO CONTINUE
+
+REM - TOOL CHAIN ERROR MESSAGE
+:ERROR1
+COLOR 1C
+CLS
 ECHO.
+ECHO ------------------------------
+ECHO       TOOL CHAIN ERROR
+ECHO ------------------------------
+ECHO :: %APP%Was Not Found.
+ECHO.
+ECHO Please check your tool chain
+ECHO PATH variables and re-start
+ECHO    WSJT Env Terminal
+ECHO.
+PAUSE
+ENDLOCAL
+EXIT /B 1
+
+REM - CONTUNE MAIN SCRIPT
+:CONTINUE
+CLS
+ECHO.
+ECHO ---------------------------
 ECHO Build Environment is Using
 ECHO ---------------------------
- cmake --version |findstr "cmake"
- qmake --version |findstr "QMake"
- svn --version |findstr "svn,"
- mingw32-make -v |findstr "Make"
- gfortran --version |findstr "Built"
- gcc --version |findstr "gcc"
- g++ --version |findstr "g++"
+  cmake --version |findstr "cmake"
+  qmake --version |findstr "QMake"
+  svn --version |findstr "svn,"
+  mingw32-make -v |findstr "Make"
+  gfortran --version |findstr "Built"
+  gcc --version |findstr "gcc"
+  g++ --version |findstr "g++"
 ECHO.
 ECHO To Build WSJT-X using CMake
 ECHO ---------------------------
