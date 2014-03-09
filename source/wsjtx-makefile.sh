dc94779445f87bd8d076b88b347855a0044aaf7b
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-03-09 11:38:44 +0000

    Dev Guide Updates
    Another round of general edits.
    
    *wsjtx-makefile.sh*
    - due to ongoing work after the wsjtx_w merge, the script is hard coded
    to check out r3834.
    - when things are sorted out with the Makefile.linux, will remove the 
    hard code.
    
    :: @Q6600 UB64-SRV1 -> Gnome Terminal
    
    
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/doc/dev-guide@3851 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- source/wsjtx-makefile.sh
+++ source/wsjtx-makefile.sh
@@ -1,60 +1,63 @@
 #!/usr/bin/env bash
 
 # Part of the WSJT Documentation Project
-# Usage:  chmod +x ./wsjtx-compile.sh && ./wsjtx-compile.sh
+# Usage:  chmod +x ./wsjtx-makefile.sh && ./wsjtx-makefile.sh
 
 # EXIT IF ERRORS
 set -e
 
 # SET WSJT-ENV DIRECTORY
-BUILD_DIR=~/wsjt-env/wsjtx-build
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
+BASE_DIR=$HOME/wsjt-env
+mkdir -p $BASE_DIR/src
+SRC_DIR=$BASE_DIR/src
+
+# MAKE DIRECTORIES
+clear && echo Creating WSJT Environment
 
 # CHECKOUT WSJT-X
-clear && echo 'Getting Latest WSJT-X Version'
-svn co svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx
-cd ./wsjtx
-rev_num=$(grep -i "rev=" mainwindow.cpp |awk '{print $3}')
+echo Getting Latest WSJT-X Version
+svn co -r 3834 svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx $SRC_DIR/wsjtx
+
+# GET CURRENT REVISION NUMBER
+rev_num=$(grep -i "rev=" $SRC_DIR/wsjtx/mainwindow.cpp |awk '{print $3}')
 
 # MAIKEFILE
-cd ./lib
+echo Building WSJTX-$rev_num
+cd $SRC_DIR/wsjtx/lib
 make -f Makefile.linux
 
 # QMAKE && MAKE
-cd ../
+cd $SRC_DIR/wsjtx
 export QT_SELECT=qt5
 qmake
 j_c=$(grep -c ^processor /proc/cpuinfo)
 make -j$j_c
 
 # MOVE WSJT-X FOLDER
-cd ../
-
-# Test for multiple builds of same $rev_num
-# For manual entry, Copy & Paste from if .. fi as one command, then [ENTER]
-if test -d ./wsjtx-$rev_num
+if test -d $BASE_DIR/wsjtx-$rev_num
 then
-mv ./wsjtx-$rev_num ./wsjtx-$rev_num-$(date +%F-%H%M)
+mv $BASE_DIR/wsjtx-$rev_num $BASE_DIR/wsjtx-$rev_num-$(date +%F-%H%M)
 fi
-
-mv ./wsjtx_install ./wsjtx-$rev_num
-cd ./wsjtx-$rev_num
+mv $SRC_DIR/wsjtx_install $BASE_DIR/wsjtx-$rev_num
 
 # COPY FILES
-cp ../wsjtx/*.dat ../wsjtx/*.txt ./
-cp -R ../wsjtx/Palettes/ ../wsjtx/samples/ ./
+cp $SRC_DIR/wsjtx/{*.dat,*.txt} $BASE_DIR/wsjtx-$rev_num/
+cp -R $SRC_DIR/wsjtx/{Palettes/,samples/} $BASE_DIR/wsjtx-$rev_num/
 rm ./CMake*
 
 # DOWNLOAD KVASD
 wget http://www.physics.princeton.edu/pulsar/K1JT/kvasd
 chmod +x ./kvasd
 
-# CLEAN-UP
-rm -rf ../wsjtx
+# CLEAN UP AFTER MAKE
+cd $SRC_DIR/wsjtx/lib && make -f Makefile.linux clean
+cd ../ && make clean
+
+# EXIT
 clear
-echo WSJTX-$rev_num Build Complete
+echo "WSJTX-$rev_num Build Complete"
+echo 'To run WSJT-X: cd' $BASE_DIR/wsjtx-$rev_num
+echo 'Then Type: ./wsjtx'
 echo
 exit 0
-
 
