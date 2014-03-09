336cc75acc230a7ba716f7d5e0ba0ecd20fc7d3c
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-03-09 06:03:29 +0000

    Added WSJT-X Linux CMake Script
    Dev Guide Updates
    
    *wsjtx-cmake.sh*
    - added wsjtx-cmake.sh file dev-guide/source
    - builds Release or Debug using $1 -r or -d
    - if $1 is null "" builds release
    - creates all dir's, build and install {Debug,Release} == $OPTION
    - $HOME/Projects/wsjt-env == $BASE_DIR
    - $BASE_DIR/src for checkout
    - moved finished $OPTION\bin to wsjt-env\wsjtx-$rev_num
    
    *wsjtx-makefile.sh*
    - copied wsjt-compile.sh to wsjtx-makefile.sh
    
    *wsjtx-.adoc*
    - removed manual steps, was too long. added notes in the script
    - rearranged most of the Linux sections
    - updated dependencies section
    - removed svn export --force so folks can svn up to the latest script.
    
    *pkgmatrix.adoc*
    - removed QT4 and QWT, as they are no longer needed.
    
    :: @Q6600 UB64-SRV1 -> Gnome Terminal
    
    
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/doc@3849 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ dev-guide/source/wsjtx-makefile.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Part of the WSJT Documentation Project
+# Usage:  chmod +x ./wsjtx-compile.sh && ./wsjtx-compile.sh
+
+# EXIT IF ERRORS
+set -e
+
+# SET WSJT-ENV DIRECTORY
+BUILD_DIR=~/wsjt-env/wsjtx-build
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+# CHECKOUT WSJT-X
+clear && echo 'Getting Latest WSJT-X Version'
+svn co svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx
+cd ./wsjtx
+rev_num=$(grep -i "rev=" mainwindow.cpp |awk '{print $3}')
+
+# MAIKEFILE
+cd ./lib
+make -f Makefile.linux
+
+# QMAKE && MAKE
+cd ../
+export QT_SELECT=qt5
+qmake
+j_c=$(grep -c ^processor /proc/cpuinfo)
+make -j$j_c
+
+# MOVE WSJT-X FOLDER
+cd ../
+
+# Test for multiple builds of same $rev_num
+# For manual entry, Copy & Paste from if .. fi as one command, then [ENTER]
+if test -d ./wsjtx-$rev_num
+then
+mv ./wsjtx-$rev_num ./wsjtx-$rev_num-$(date +%F-%H%M)
+fi
+
+mv ./wsjtx_install ./wsjtx-$rev_num
+cd ./wsjtx-$rev_num
+
+# COPY FILES
+cp ../wsjtx/*.dat ../wsjtx/*.txt ./
+cp -R ../wsjtx/Palettes/ ../wsjtx/samples/ ./
+rm ./CMake*
+
+# DOWNLOAD KVASD
+wget http://www.physics.princeton.edu/pulsar/K1JT/kvasd
+chmod +x ./kvasd
+
+# CLEAN-UP
+rm -rf ../wsjtx
+clear
+echo WSJTX-$rev_num Build Complete
+echo
+exit 0
+
+
