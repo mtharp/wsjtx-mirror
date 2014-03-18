61e77eae1e02e5ccd00cfa1bef06f7ffac018e92
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-03-18 16:30:36 +0000

    Remove hard-coded paths.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/trunk@3880 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- g3.bat
+++ g3.bat
@@ -1,5 +1,4 @@
-set INSTALLDIR=install
-rm -rf %INSTALLDIR%
-mkdir %INSTALLDIR%
-mkdir %INSTALLDIR%\bin
-c:\Python33\Scripts\cxfreeze --include-path=. --include-modules=Pmw wsjt.py --target-dir=%INSTALLDIR%\bin
+rm -rf install
+mkdir install
+mkdir install\bin
+cxfreeze --include-path=. --include-modules=Pmw wsjt.py --target-dir=install\bin
--- g4.bat
+++ g4.bat
@@ -1,5 +1,4 @@
-set INSTALLDIR=install
-rm -rf %INSTALLDIR%/bin/tcl/tzdata
-rm -rf %INSTALLDIR%/bin/tk/demos
-cp -r RxWav %INSTALLDIR%
-cp CALL3.TXT kvasd.dat kvasd.exe wsjt.ico wsjt10.bat %INSTALLDIR%
+rm -rf install/bin/tcl/tzdata
+rm -rf install/bin/tk/demos
+cp -r RxWav install
+cp CALL3.TXT kvasd.dat kvasd.exe wsjt.ico wsjt10.bat install
