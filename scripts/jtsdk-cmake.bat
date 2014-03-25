669e95fcf919fb7675a7d178d58d2affb7eabd94
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-03-25 16:00:18 +0000

    Initial commit, still working on updates
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/doc/dev-guide@3916 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ scripts/jtsdk-cmake.bat
@@ -0,0 +1,231 @@
+@ECHO OFF
+REM -- JTSDK-QT Windows CMake Build Script
+REM -- Part of the JTSDK Project
+CLS
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
+COLOR 1B
+
+REM -- TEST DOUBLE CLICK
+FOR %%x IN (%cmdcmdline%) DO IF /I "%%~x"=="/c" SET GUI=1
+IF DEFINED GUI GOTO DCLICKERROR
+
+REM -- START MAIN SCRIPT
+SET TARGET=%~dp0
+IF %TARGET:~-1%==\ (SET BASED=%TARGET:~0,-1%) ELSE (SET BASED=%TARGET%) 
+
+REM -- USER INPUT FILED 1 = %1
+SET SUPPORTED=(map65 wsjtx wsprx)
+IF /I [%1]==[wsjtx] (SET APP_NAME=wsjtx
+) ELSE IF /I [%1]==[wsprx] (SET APP_NAME=wsprx
+) ELSE IF /I [%1]==[map65] (SET APP_NAME=map65
+) ELSE (GOTO UNSUPPORTED)
+
+REM - USER INPUT FIELD 2 == %2
+IF /I [%2]==[-d] (SET OPTION=Debug) ELSE (SET OPTION=Release)
+
+REM -- SET PATH VARS
+SET SVND=%BASED%\subversion\bin
+SET CMAKD=%BASED%\cmake\bin
+SET HAMLIBD=%BASED%\hamlib
+SET GCCD=%BASED%\mingw48_32\bin
+SET QT5D=%BASED%\qt5\5.2.1\mingw48_32\bin
+SET SRCD=%BASED%\src
+SET TOOLS=%BASED%\tools
+SET SCRIPTS=%TOOLS%\scripts
+SET PATH=%BASED%;%SVND%;%CMAKED%;%GCCD%;%QT5D%;%SRCD%;%TOOLS%;%SCRIPTS%;%WINDIR%;%WINDIR%\System32
+
+REM - MISC VARS
+SET JJ=%NUMBER_OF_PROCESSORS%
+SET TCHAIN=%BASED%\jtsdk-toolchain.cmake
+SET APP_DIR=%BASED%\%APP_NAME%
+SET BUILDD=%BASED%\%APP_NAME%\build
+SET INSTALLD=%BASED%\%APP_NAME%\install
+SET SUPPORT=%BASED%\appsupport
+
+
+REM -- START MAIN BUILD
+CD %BASED%
+mkdir %SRCD%
+CLS
+ECHO -------------------------------
+ECHO %APP_NAME% CMake Build Script
+ECHO -------------------------------
+ECHO.
+
+REM -- CHECK IN %APP_NAME\.svn IS PRESENT
+IF NOT EXIST %SRCD%\%APP_NAME%\NUL (
+ECHO.
+ECHO CHECKING OUT: ^( %APP_NAME% ^)
+ECHO.
+CD /D %SRCD%
+start /wait svn co svn://svn.code.sf.net/p/wsjt/wsjt/branches/%APP_NAME%
+CD /D %BASED%
+GOTO BUILD
+) ELSE (
+GOTO SVNASK
+)
+
+REM -- ASK TO UPDATE FROM SVN
+:SVNASK
+ECHO Update from SVN Before Building? ^( y/n ^)
+SET ANSWER=
+ECHO.
+SET /P ANSWER=Type Responce: %=%
+If /I "%ANSWER%"=="N" GOTO BUILD
+If /I "%ANSWER%"=="Y" (
+GOTO SVNUP
+) ELSE (
+CLS
+ECHO.
+ECHO Please Answer With: ^( Y or N ^) & ECHO. & GOTO ASK
+)
+
+:SVNUP
+ECHO.
+ECHO UPDATING %SRCD%\%APP_NAME%
+ECHO.
+CD /D %SRCD%\%APP_NAME%
+start /wait svn update
+CD /D %BASED%
+ECHO.
+
+:BUILD
+IF NOT EXIST %BUILDD%\%OPTION%\NUL mkdir %BUILDD%\%OPTION%
+CD %BUILDD%\%OPTION%
+ECHO.
+ECHO Starting Build For: ^( %APP_NAME% ^)
+ECHO.
+COLOr 1B
+cmake -G "MinGW Makefiles" ^
+-DCMAKE_COLOR_MAKEFILE=OFF ^
+-DCMAKE_TOOLCHAIN_FILE=%TCHAIN% ^
+-DCMAKE_BUILD_TYPE=%OPTION% ^
+-DCMAKE_INSTALL_PREFIX=%INSTALLD%/%OPTION% %SRCD%/%APP_NAME% 
+COLOR 1B
+
+REM -- target \JTSDK\%APP_NAME%\install\{Release, Debug}\bin
+mingw32-make -j%JJ% install
+COLOR 1B
+GOTO CPFILES
+
+REM -- POST BUILD COPY - BASED APP TYPE
+:CPFILES
+SET CPTXT=*.txt *.dat *.conf *.ini
+SET RBCP=ROBOCOPY /NS /NC /NFL /NDL /NP /NJS /NJH
+%RBCP% %SRCD%\%APP_NAME% %INSTALLD%\%OPTION%\bin %CPTXT% /XF CMake*
+cp -r %SUPPORT%\%APP_NAME%\* %INSTALLD%\%OPTION%\bin
+cp -r %SUPPORT%\runtime\* %INSTALLD%\%OPTION%\bin
+
+REM -- MAKE NEEDED DIRECTORY
+IF NOT EXIST %INSTALLD%\%OPTION%\bin\save\Samples (
+mkdir %INSTALLD%\%OPTION%\bin\save\Samples)
+GOTO FINISHED
+
+REM - TOOL CHAIN ERROR MESSAGE
+:UNSUPPORTED
+CLS
+ECHO.
+ECHO ------------------------------
+ECHO    UNSUPPORTED APPLICATION
+ECHO ------------------------------
+ECHO Currently, ^( %1 ^) CMake build 
+ECHO        is not Supported
+ECHO.
+ECHO.
+PAUSE
+CLS
+CALL %SCRIPTS%\jtsdk-qtinfo.bat
+EXIT /B 0
+
+REM - DOUBLE-CLICK ERROR MESSAGE
+:DCLICKERROR
+CLS
+@ECHO OFF
+ECHO -------------------------------
+ECHO        Execution Error
+ECHO -------------------------------
+ECHO.
+ECHO Please Run from JTSDK Enviroment
+ECHO.
+ECHO  Use: %~dp0\jtsdk-env.bat
+ECHO.
+PAUSE
+GOTO EOF
+
+REM - COMPILER ERRORS
+:BUILDRROR
+CLS
+@ECHO OFF
+ECHO -------------------------------
+ECHO          BUILD ERROR
+ECHO -------------------------------
+ECHO.
+ECHO    CMake Existed with Errors
+ECHO.
+PAUSE
+GOTO EOF
+
+:SVNERROR1
+CLS
+ECHO -------------------------------
+ECHO       SVN Execution Error
+ECHO -------------------------------
+ECHO.
+ECHO Subversion returned with an error.
+ECHO    ~~ Performing Cleanup ~~
+ECHO Rerun the build script after Exit.
+ECHO     If the problem continues
+ECHO     Contact: ki7mt@yahoo.com
+PAUSE
+CD /D %SRCD%\%APP_NAME%
+svn cleanup
+CLS
+ECHO -------------------------------
+ECHO       Cleanup Complete
+ECHO -------------------------------
+ECHO.
+ECHO         Now exiting
+sleep 2
+GOTO EOF
+
+:FINISHED
+ECHO.
+ECHO -------------------------------
+ECHO   %APP_NAME% Build Complete
+ECHO -------------------------------
+ECHO.
+ECHO File Locaiton: %INSTALLD%\%OPTION%\bin\%APP_NAME%.exe
+ECHO.
+GOTO ASKRUN
+
+:ASKRUN
+ECHO Would You Like To Run %APP_NAME% Now? ^( y/n ^)
+ECHO.
+SET ANSWER=
+SET /P ANSWER=Type Responce: %=%
+ECHO.
+If /I "%ANSWER%"=="Y" GOTO RUNAPP
+If /I "%ANSWER%"=="N" (
+REM -- CLS
+REM -- CALL %SCRIPTS%\jtsdk-qtinfo.bat
+GOTO EOF
+) ELSE (
+CLS
+ECHO.
+ECHO Please Answer With: ^( y or n ^) & ECHO. & GOTO ASKRUN
+)
+GOTO EOF
+
+:RUNAPP
+ECHO.
+CD /D %INSTALLD%\%OPTION%\bin
+ECHO Starting: %APP_NAME%
+START %APP_NAME%.exe
+GOTO EOF
+
+:EOF
+CD /D %BASED%
+REM -- CALL %SCRIPTS%\jtsdk-qtinfo.bat
+ENDLOCAL
+EXIT /B 0
---
+++ scripts/jtsdk-docenv.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+REM -- JTSDK-DOC Environment
+REM -- Part of the WSJT Documentation project
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
+COLOR F0
+TITLE JTSDK-DOC Environment
+
+SET TARGET=%~dp0
+IF %TARGET:~-1%==\ SET TARGET=%TARGET:~0,-1%
+SET BASED=%TARGET%
+SET SVND=%BASED%\subversion\bin
+SET TOOLS=%BASED%\tools
+SET PATH=%BASED%;%SVND%;%TOOLS%;%WINDIR%;%WINDIR%\System32
+CD /D %BASED%
+
+IF EXIST %BASED%\doc (
+call %BASED%\doc\doc-env.bat
+) ELSE (
+CLS
+ECHO -----------------------------------
+ECHO     Doc Directory Not Found
+ECHO -----------------------------------
+ECHO.
+ECHO In order to use the build keys, you
+ECHO must firtst perform a checkout from
+ECHO SourceForge, then relaunch jtsdk-docenv.bat:
+ECHO.
+ECHO Anonymous .. Type: svn co svn://svn.code.sf.net/p/wsjt/wsjt/branches/doc
+ECHO Developer .. Type: svn co https://%USERNAME%@svn.code.sf.net/p/wsjt/wsjt/branches/doc
+ECHO.
+ECHO Note: For Dev's, replace %USERNAME% with your SorceForge User Name.
+ECHO.
+pause
+ECHO.
+%WINDIR%\System32\cmd.exe /A /Q /K
+)
