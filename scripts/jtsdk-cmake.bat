60f0bd9335231062fa3897cb69644b722e8a5c15
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-03-27 13:12:25 +0000

    update GCC paths for new JTSDK-QT
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/doc/dev-guide@3936 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- scripts/jtsdk-cmake.bat
+++ scripts/jtsdk-cmake.bat
@@ -15,7 +15,7 @@ SET BASED=%~dp0
 IF %BASED:~-1%==\ SET BASED=%BASED:~0,-1%
 SET SVND=%BASED%\subversion\bin
 SET CMAKD=%BASED%\cmake\bin
-SET GCCD=%BASED%\mingw48_32\bin
+SET GCCD=%BASED%\qt5\Tools\mingw48_32\bin
 SET QT5D=%BASED%\qt5\5.2.1\mingw48_32\bin
 SET SRCD=%BASED%\src
 SET TOOLS=%BASED%\tools
