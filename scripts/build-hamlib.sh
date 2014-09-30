b22bb1d8018721b863c621d89668ec706d840177
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-09-30 20:15:18 +0000

    JTSDK-QT more build-hamlib.sh updates
    After several more iterations, and more guidance from Bill, the
    following looks to be a valid solution.
    
    Originally, I posted the "WRONG" MSYS link. It uses MSYS from
    MinGW-Builds project, *not MSYS2*, apologias for the confusion.
    
    MSYS LINK: http://sourceforge.net/projects/mingwbuilds/files/external-binary-packages/
    PACKAGE: msys+7za+wget+svn+git+mercurial+cvs-rev13.7z 2013-05-15
    
    MSYS INSTALL & SETUP
    -Download MSYS 7z package
    -Extract to c:\msys32
    -There should *not be* an installer, if so, it is the wrong package.
    -Browse too, then open: c:\msys32\msys\msys.bat
    
    NOTE: The first run sets up your user profile. You may need to restart
    MSYS after. I have it installed to d:\msys32. I tested c:\msys32 and saw
    no differences to speak of.
    
    DOWNLOAD BUILD SCRIPT
    svn export https://svn.code.sf.net/p/wsjt/wsjt/branches/doc/dev-guide/scripts/build-hamlib.sh
    
    BUILD G4WJS HAMLIB3 with INTEGRATION
    ./build-hamlib.sh
    
    BUILD AND CREATE A BUILD-LOG
    ./build-hamlib.sh |tee -a ./hamlib-build.log
    
    
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/doc/dev-guide@4413 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- scripts/build-hamlib.sh
+++ scripts/build-hamlib.sh
@@ -5,47 +5,40 @@
 # Requirements: MSYS Env, Autotools, Git, Bash
 #               Qt5 Toolchain ( 5.2.1 tested with C/C++ 4.8.0 )
 
-# MSYS2 includes: Git, SVN, Hg, Autotools, Bash, Make and many GNU Tools
-# MSYS2 Link: http://hivelocity.dl.sourceforge.net/project/mingwbuilds/external-binary-packages/msys%2B7za%2Bwget%2Bsvn%2Bgit%2Bmercurial%2Bcvs-rev13.7z
+# MSYS includes: Git, SVN, Autotools, Bash, Make and many GNU Tools
+# MSYS Link: http://sourceforge.net/projects/mingwbuilds/files/external-binary-packages/
+# PACKAGE: msys+7za+wget+svn+git+mercurial+cvs-rev13.7z 2013-05-15
 
 # Exit on errors
 set -e
 
-# Date an create build directory
+# Date, build and tool-chain paths
 today=$(date +"%m-%d-%y")
+PATH="/c/JTSDK-QT/qt5/Tools/mingw48_32/bin:$PATH"
 mkdir -p ~/g4wjs-hamlib/build
-
-# Path to Compilers & Binutils, in JTSDK-QT
-# If you change the path, check the autogen section to ensure each
-# item can be found, otherwise you may get errors.
-TOOLS="C:/JTSDK-QT/qt5/Tools/mingw48_32/bin"
 
 # For JTSDK-QT use, this should remain as is. If you change this path,
 # also update jtsdk.toolchain.cmake file to match the new location.
-INSTALLPREFIX="C:/JTSDK-QT/hamlib3/mingw32"
+INSTALLPREFIX=C:/JTSDK-QT/hamlib3/mingw32
 
 # Simple exit on error function
 function exit_status() {
 
-	if [ "$?" != "0" ];
+	if [[ "$?" != "0" ]];
 	then
 		exit 1
 	fi
 }
 
-echo ''
-echo '----------------------------------------------------------------'
-echo '  CLONE G4WJS HAMLIB3 & CHECKOUT INTEGRATION'
-echo '----------------------------------------------------------------'
-echo ''
-echo '.. Be patient, this can take a few minutes'
-echo ''
+echo
+echo "----------------------------------------------------------------"
+echo "  CLONE G4WJS HAMLIB3 & CHECKOUT INTEGRATION"
+echo "----------------------------------------------------------------"
+echo
+echo ".. Be patient, this can take a few minutes"
+echo 
 
-# Note: Rather than perform a full download each run, the script
-# deletes the build directory instead. If autogeh.sh exists, it will
-# perform a pull & integration rather than clone & integration
-
-if [ -f ~/g4wjs-hamlib/src/autogen.sh ];
+if [[ -f ~/g4wjs-hamlib/src/autogen.sh ]];
 then
 	cd ~/g4wjs-hamlib/src
 	git pull
@@ -59,20 +52,16 @@ else
 	exit_status
 fi
 
-echo ''
-echo '----------------------------------------------------------------'
-echo '  RUN AUTOGEN TO CONFIGURE THE BUILD'
-echo '----------------------------------------------------------------'
-echo ''
-echo '.. Be patient, this can take a few minutes'
-echo ''
+echo
+echo "----------------------------------------------------------------"
+echo "  RUN AUTOGEN TO CONFIGURE THE BUILD"
+echo "----------------------------------------------------------------"
+echo
+echo ".. Be patient, this can take a few minutes"
+echo
 
 # Running autogen.sh to configure the build. No need to run configure
 # as autogen.sh accepts $@
-
-# Note: It *may not* be necessary to set each of the Binutil items, but
-# to ensure the build uses the same binary set as the compilers,
-# each has been set in the autogen.sh invocation
 
 cd ~/g4wjs-hamlib/build
 ../src/autogen.sh --prefix="$INSTALLPREFIX" \
@@ -81,60 +70,47 @@ cd ~/g4wjs-hamlib/build
 CC=C:/JTSDK-QT/qt5/Tools/mingw48_32/bin/gcc \
 CXX=C:/JTSDK-QT/qt5/Tools/mingw48_32/bin/g++ \
 CFLAGS="-fdata-sections -ffunction-sections" \
-LDFLAGS="-s -Wl,--gc-sections" \
-ADDR2LINE=$TOOLS/addr2line AR=$TOOLS/ar DLLTOOL=$TOOLS/dlltool \
-DLLWRAP=$TOOLS/dllwrap GPROF=$TOOLS/gprog GDB=$TOOLS/gdb LD=$TOOLS/ld \
-NM=$TOOLS/nm OBJCOPY=$TOOLS/objcopy OBJDUMP=$TOOLS/objdump \
-RANLIB=$TOOLS/ranlib READELF=$TOOLS/readelf SIZE=$TOOLS/size S\
-TRIP=$TOOLS/strip STRINGS=$TOOLS/strings WINDRES=$TOOLS/windres \
-WINDMC=$TOOLS/windmc
+LDFLAGS="-s -Wl,--gc-sections"
 exit_status
 
 # Make clean check
-if [ -f ~/g4wjs-hamlib/build/tests/rigctld.exe ];
+if [[ -f ~/g4wjs-hamlib/build/tests/rigctld.exe ]];
 then
-	echo ''
+	echo
 	echo '----------------------------------------------------------------'
 	echo '  RUNNING MAKE CLEAN'
 	echo '----------------------------------------------------------------'
-	echo ''
+	echo
 	make -s clean
 fi
 
 # Run Make
-echo ''
-echo '----------------------------------------------------------------'
+echo
+echo "----------------------------------------------------------------"
 echo '  RUNNING MAKE'
-echo '----------------------------------------------------------------'
-echo ''
+echo "----------------------------------------------------------------"
+echo
 make -s
 exit_status
 
-# Note: If you use the JTSDJ-DOC update method, do not remove the
-# build-date-$today file from JTSDK-QT\hamlib3 directory, otherwise,
-# the update script will overwrite your latest changes with that which
-# resides in SVN. Instead, make a backup of the hamlib3 directory
-# before hand, then reinstate it after testing a new Halib3 build
-# if your concerned about loosing the original build.
-
 # Run Make Install
-echo ''
-echo '----------------------------------------------------------------'
-echo '  INSTALLING HAMLIB3'
-echo '----------------------------------------------------------------'
-echo ''
+echo
+echo "----------------------------------------------------------------"
+echo "  INSTALLING HAMLIB3"
+echo "----------------------------------------------------------------"
+echo
 
 make -s install
 exit_status
-touch C:/JTSDK-QT/hamlib3/build-date-$today
+touch C:/JTSDK-QT/hamlib3/build-date-"$today"
 exit_status
 
-echo ''
-echo '----------------------------------------------------------------'
-echo '  FINISHED HAMLIB3 BUILD'
-echo '----------------------------------------------------------------'
-echo ''
+echo
+echo "----------------------------------------------------------------"
+echo "  FINISHED HAMLIB3 BUILD"
+echo "----------------------------------------------------------------"
+echo 
 echo "Install Location: $INSTALLPREFIX"
-echo ''
+echo
 
 exit 0
