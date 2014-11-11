d20a66564438a65419038b4a1a914d2f85892aca
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-11-11 23:51:52 +0000

    Update KVASD-Installer
    * Added auto build scripts
    * updated downloads for kvasd based on $ARCH (Linux Only)
    * Minor updates to other scripts
    * Wrote a freash conrigure.ac & Makefile.in
    * added Autogen.sh sctipt to run autoconfi -f -i
    * Added Install / Uninstall targets to configure.ac
    
    TO-DO
    * Add *darwin* and / other $host_os download
    * Decide on whether to use WGET or Curl for the download app
    
    
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/trunk/kvasd-binary/kvasd-installer@4589 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ autogen.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+#
+#-------------------------------------------------------------------------------
+# This file is part of the kvasd-instter application
+#
+# File Name:    autogen.sh
+# Description:  script to generate configire and makefile
+#
+# Run ./autogen.sh
+#
+# Author: Greg Beam, KI7MT <ki7mt@yahoo.com>
+# Copyright (C) 2001-2014 Joseph Taylor, K1JT
+# License: GPL-3
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+#-------------------------------------------------------------------------------
+
+set -e
+
+_BASED=$(exec pwd)
+_PROGRAM=kvasd-installer
+
+# Start main script
+cd $_BASED
+
+autoconf --version > /dev/null 2>&1
+if test "$?" -eq 1; then
+# message if autoconf was found or not "0"=OK, "1"= Not Reachable
+	clear
+	echo ''
+	echo "You must have autoconf installed to compile $_PROGRAM."
+	echo "Install the appropriate package for your distribution,"
+	echo ''
+	exit 1
+fi
+
+clear
+echo ''
+echo "-------------------------------------------"
+echo " Running Autotools for: $_PROGRAM"
+echo "-------------------------------------------"
+echo ''
+echo "Running ( autoconf -f -i ) to process configure.ac"
+
+# Generate configure script from configure.ac and aclocal.m4
+autoconf -f -i
+
+# simple test for the configure script, after running autogen.sh
+if test -s ./configure; then
+	echo "Finished autoconf .."
+	echo "Building the Makefile .."
+	sleep 1
+else
+# message if configure was not found
+	echo
+	echo "There was a problem generating the configure script"
+	echo "Check config.status for details."	
+	echo
+	exit 1
+fi
+
+# message if no arguments were presented
+if test -z "$*"; then
+	echo "Using ./configure <defualts>"
+	sleep 1
+else
+# List user input arguments
+	echo "Using ./configure $@"
+	echo ''
+	sleep 2
+fi
+
+$_BASED/configure "$@"
+
+exit 0
