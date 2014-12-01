2030b49ac7dc4b8fff49dd2508726634f1628529
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2014-12-01 03:30:35 +0000

    WSPR update fmt-env and gocal.bat files
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wspr@4708 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- fmt-env.bat
+++ fmt-env.bat
@@ -1,36 +1,58 @@
+::-----------------------------------------------------------------------------::
+:: Name .........: fmt-env.bat
+:: Project ......: Part of the WSPR Project
+:: Description ..: Maintenance script for updated & upgrades or general use
+:: Project URL ..: http://sourceforge.net/projects/jtsdk
+:: Usage ........: Run this file directly, or from the Windows Start Menu
+::
+:: Author .......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+:: Copyright ....: Copyright (C) 2001-2014 Joe Taylor, K1JT
+:: License ......: GPL-3
+::
+:: fmt-env.bat is free software: you can redistribute it and/or modify it under
+:: the terms of the GNU General Public License as published by the Free Software
+:: Foundation either version 3 of the License, or (at your option) any later
+:: version. 
+::
+:: fmt-ent.bat is distributed in the hope that it will be useful, but
+:: WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+:: or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+:: more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+::-----------------------------------------------------------------------------::
+
+REM -- This file sets the paths for ( fmtest, gocal.bat, wspr0 and wsprcode ).
+REM    It does *not* permanently alter System or User %PATH%. Paths are re-set
+REM    by closing the CMD window.
+
 @ECHO OFF
-REM -- Fmtest and WSPR0 Environment
-REM    This file is used with the WSPR InnoSetup Win32 Installer
-REM    It sets paths to the required DLL's based on the
-REM    application install directory. It Does *not* permanently 
-REM    alter System or User %PATH%. Paths are re-set by closing
-REM    the CMD window.
-SETLOCAL ENABLEEXTENSIONS
-SETLOCAL ENABLEDELAYEDEXPANSION
+SETLOCAL
 COLOR 0B
-TITLE FMTEST Environment
+TITLE FMTEST - WSPR Code Environment
 
 REM -- SETUP PATHS
 PATH=.;.\bin
-COPY /N /Y /B bin\libgcc* .
-COPY /N /Y /B bin\libquad* .
-COPY /N /Y /B bin\libgfortran* . 
-COPY /N /Y /B bin\pthreadGC2* .
 CLS
-ECHO -----------------------------------------------------------------
+ECHO --------------------------------------------------------------
 ECHO  Welcome to WSJT's FMTEST / WSPR0 Tool Suite
-ECHO -----------------------------------------------------------------
+ECHO --------------------------------------------------------------
 ECHO.
-ECHO  Available Apps: fmtest fmtave fmeasure fcal wspr0 wsprcode
-ECHO.
-ECHO  For Help, type: ^( app-name ^) then ENTER
+ECHO  Applications ....: fmtest fmtave fmeasure fcal wspr0 wsprcode
+ECHO  For Help, type ..: ^( app-name ^) then ENTER
 ECHO.
 IF NOT EXIST "%~dp0\WSPR.INI" (
 ECHO.
-ECHO  CAUTION: Before running Fmtest or Gocal.bat, you must first
+ECHO  WSPR.INI Required
+ECHO  -----------------
+ECHO  Before running ^( fmtest or gocal.bat ^), you must first
 ECHO  run WSPR to generate an WSPR.INI file.
+ECHO.
+ECHO  Make sure to save your latest changes by going to: 
+ECHO  File, Save user parameters
 ECHO.
 )
 
 :: OPEN CMD WINDOW
-%WINDIR%\System32\cmd.exe /A /Q /K
+%COMSPEC% /A /Q /K
--- gocal.bat
+++ gocal.bat
@@ -1,11 +1,36 @@
+::-----------------------------------------------------------------------------::
+:: Name .........: gocal.bat
+:: Project ......: Part of the WSPR Project
+:: Description ..: Maintenance script for updated & upgrades or general use
+:: Project URL ..: http://sourceforge.net/projects/jtsdk
+:: Usage ........: Run this file directly, or from the Windows Start Menu
+::
+:: Author .......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+:: Copyright ....: Copyright (C) 2001-2014 Joe Taylor, K1JT
+:: License ......: GPL-3
+::
+:: gocal.bat is free software: you can redistribute it and/or modify it under
+:: the terms of the GNU General Public License as published by the Free Software
+:: Foundation either version 3 of the License, or (at your option) any later
+:: version. 
+::
+:: gocal.bat is distributed in the hope that it will be useful, but WITHOUT ANY
+:: WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+:: A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+::-----------------------------------------------------------------------------::
+
 @ECHO OFF
-REM - Batch file to run gocat.bat for Fmtest
-SET PATH=%PATH%;%~dp0\bin
+SETLOCAL
+PATH=.;.\bin
 IF NOT EXIST "%~dp0\WSPR.INI" ( GOTO NEED_INI )
 
 REM -- Only edit items "between" < BEGIN .. and .. END >
+REM    More Info: http://physics.princeton.edu/pulsar/K1JT/FMT_User.pdf
 
-:: BEGIN - Edit Station Information
+:: < BEGIN - Edit Station Information >
 fmtest   660 1 1500 100 30  WFAN
 fmtest   880 1 1500 100 30  WCBS
 fmtest  1210 1 1500 100 30  WPHT
@@ -17,19 +42,25 @@ fmtest 10000 1 1500 100 30  WWV
 fmtest 14670 1 1500 100 30  CHU
 fmtest 15000 1 1500 100 30  WWV
 fmtest 20000 1 1500 100 30  WWV
-:: END - Edit Station Information
+:: < END - Edit Station Information >
 GOTO EOF
 
 :EOF
+ENDLOCAL
 EXIT /B 0
 
 :NEED_INI
 CLS
-ECHO -----------------------------------
-ECHO       Missing WSPR.INI File
-ECHO -----------------------------------
-ECHO You must first run WSPR to generate
-ECHO the an WSPR.INI file before running
-ECHO gocal.bat
+ECHO ------------------------------------
+ECHO        Missing WSPR.INI File
+ECHO ------------------------------------
+ECHO  You must first run WSPR to generate
+ECHO  the an WSPR.INI file before running
+ECHO  gocal.bat
 ECHO.
+ECHO  Make sure to save your latest
+ECHO  changes by going to: 
+ECHO  File, Save user parameters
+ECHO.
+ENDLOCAL
 EXIT /B 1
