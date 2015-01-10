cce998f172cac9135d4bb33547bc89b517ea9bde
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2015-01-10 21:36:44 +0000

    kvasd-installer minor text updates
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/trunk/kvasd-binary/kvasd-installer@4892 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- autogen.sh
+++ autogen.sh
@@ -3,14 +3,15 @@
 #-------------------------------------------------------------------------------
 # This file is part of the kvasd-instter application
 #
-# File Name:    autogen.sh
-# Description:  script to generate configire and makefile
+# File Name ....: autogen.sh
+# Description ..: script to generate configire and makefile
+# Execution ....: ./autogen.sh
 #
-# Run ./autogen.sh
-#
-# Author: Greg Beam, KI7MT <ki7mt@yahoo.com>
-# Copyright (C) 2001-2014 Joseph Taylor, K1JT
-# License: GPL-3
+# Author .......: Greg Beam, KI7MT <ki7mt@yahoo.com>
+# Copyright ....: Copyright (C) 2014-2015 Joseph Taylor, K1JT
+# License ......: GPL-3+
+# Website ......: http://physics.princeton.edu/pulsar/k1jt/devel.html
+# Report Bugs ..: wsjt-devel@lists.sourceforge.net
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -30,18 +31,18 @@
 
 set -e
 
-_BASED=$(exec pwd)
-_PROGRAM=kvasd-installer
+BASED=$(exec pwd)
+PROGRAM='KVASD Installer'
 
 # Start main script
-cd $_BASED
+cd $BASED
 
 autoconf --version > /dev/null 2>&1
 if test "$?" -eq 1; then
 # message if autoconf was found or not "0"=OK, "1"= Not Reachable
 	clear
 	echo ''
-	echo "You must have autoconf installed to compile $_PROGRAM."
+	echo "You must have autoconf installed to compile $PROGRAM."
 	echo "Install the appropriate package for your distribution,"
 	echo ''
 	exit 1
@@ -50,7 +51,7 @@ fi
 clear
 echo ''
 echo "-------------------------------------------"
-echo " Running Autotools for: $_PROGRAM"
+echo " Running Autotools for: $PROGRAM"
 echo "-------------------------------------------"
 echo ''
 echo "Running ( autoconf -f -i ) to process configure.ac"
@@ -61,13 +62,11 @@ autoconf -f -i
 # simple test for the configure script, after running autogen.sh
 if test -s ./configure; then
 	echo "Finished autoconf .."
-	echo "Building the Makefile .."
-	sleep 1
 else
 # message if configure was not found
 	echo
 	echo "There was a problem generating the configure script"
-	echo "Check config.status for details."	
+	echo "Check config.status and config.log for details."	
 	echo
 	exit 1
 fi
@@ -75,14 +74,12 @@ fi
 # message if no arguments were presented
 if test -z "$*"; then
 	echo "Using ./configure <defualts>"
-	sleep 1
 else
 # List user input arguments
 	echo "Using ./configure $@"
 	echo ''
-	sleep 2
 fi
 
-$_BASED/configure "$@"
+$BASED/configure "$@"
 
 exit 0
