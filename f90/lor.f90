7cf25bab7be110232c35bca4b6980b97e2f2691a
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2015-01-18 18:27:44 +0000

    A few tweaks to the Lorentzian-fit main program.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/echo@4900 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- f90/lor.f90
+++ f90/lor.f90
@@ -8,39 +8,46 @@ program lor
   real*4 gran
 
   open(10,file='lor.dat',status='old')
-  read(10,*) nterms,mode,lambda
+  read(10,*) nterms,mode,lambda,nverbose
   read(10,*) a0
   read(10,*) a
   read(10,*) deltaa
 
-  write(*,1000) a0
-  write(*,1000) a
-  write(*,1000) deltaa
-  write(*,1000) 
-1000 format(2x,5f8.3)
+  write(*,1000)
+1000 format("       Baseline   Amp     DF     width    exp"/50("-"))
+
+  write(*,1002) "True: ",a0
+  write(*,1002) "Guess:",a
+  write(*,1002) "Dstep: ",deltaa
+  write(*,1002) 
+1002 format(a6,5f8.3)
+
+  if(nverbose.ne.0) write(*,1004)
+1004 format("  Iter  Baseline  Amp     DF     width    exp     chisq   lambda"&
+          /66("-"))
 
   do i=1,NZ
      x(i)=i-50
      y(i)=functn(x,i,a0) + gran()
-     write(13,1010) x(i),y(i)
-1010 format(2f10.4)
+!     write(13,1010) x(i),y(i)
+!1010 format(2f10.4)
   enddo
 
   sigmay=1.0
   chisqr0=1.e30
   do iter=1,20
      call curfit(x,y,sigmay,nz,nterms,mode,a,deltaa,sigmaa,lambda,yfit,chisqr)
-     write(*,1020) iter,a,chisqr,lambda
-1020 format(i2,5f8.3,f9.3,f10.6)
+     if(nverbose.ne.0) write(*,1020) iter,a,chisqr,lambda
+1020    format(i6,5f8.3,f9.3,f10.6)
      if(chisqr/chisqr0.ge.0.999d0) exit
      chisqr0=chisqr
   enddo
 
      write(*,1030) 
-     write(*,1030) a,chisqr
-     write(*,1030) sigmaa
-     write(*,1030) a-a0
-1030 format(2x5f8.3,f9.3)
+     write(*,1030) "Fit:",a,chisqr
+     write(*,1030) "Dev:",a-a0
+     write(*,1030) "Err:",sigmaa
+1030 format(a4,2x,5f8.3,f9.3)
 
   do i=1,NZ
      write(14,1040) x(i),y(i),yfit(i)
