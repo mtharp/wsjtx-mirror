03a2490f64e90ccd2175c88d34645c85d8e2ca7e
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2015-03-21 18:13:11 +0000

    Adjustments to message averaging GUI.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx_exp@5097 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- messageaveraging.cpp
+++ messageaveraging.cpp
@@ -6,6 +6,25 @@ MessageAveraging::MessageAveraging(QWidget *parent) :
   ui(new Ui::MessageAveraging)
 {
   ui->setupUi(this);
+  QList<QCheckBox*> cb;
+  cb.append(ui->cb1);
+  cb.append(ui->cb2);
+  cb.append(ui->cb3);
+  cb.append(ui->cb4);
+  cb.append(ui->cb5);
+  cb.append(ui->cb6);
+  cb.append(ui->cb7);
+  cb.append(ui->cb8);
+  cb.append(ui->cb9);
+  cb.append(ui->cb10);
+  cb.append(ui->cb11);
+  cb.append(ui->cb12);
+  cb.append(ui->cb13);
+  cb.append(ui->cb14);
+  cb.append(ui->cb15);
+  for(int i=0; i<15; i++) {
+    cb[i]->setText("     ");
+  }
 }
 
 MessageAveraging::~MessageAveraging()
