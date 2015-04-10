704547af966387d456a99141ec26a10a74b800c8
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2015-04-10 01:48:22 +0000

    Temporary test program.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx_exp@5199 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

---
+++ lib/t1.f90
@@ -0,0 +1,19 @@
+program t1
+
+  parameter (NMAX=1000000,M=52)
+  integer ic(M,NMAX)
+  integer ndist(NMAX)
+  real c(M)
+
+  do i=1,NMAX
+     call random_number(c)
+     ic(1:M,i)=int(4*c)
+  enddo
+
+  do j=1,NMAX
+     ndist(j)=count(ic(1:M,j).ne.ic(1:M,1))
+     if(ndist(j).le.10) write(13,1000) j,ndist(j)
+1000 format(2i10)
+  enddo
+
+end program t1
