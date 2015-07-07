6e9aa94634a05d8312d864de72632e678b8425df
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2015-07-07 14:21:38 +0000

    C-style termination for decoded ISCAT lines.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx_exp@5698 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/iscat.f90
+++ lib/iscat.f90
@@ -156,7 +156,7 @@ subroutine iscat(cdat0,npts0,nh,npct,t2,pick,cfile6,minsync,ntol,   &
            if(isync.ge.1) csync='*'
            if(nlines.le.maxlines-1) nlines = nlines + 1
            write(line(nlines),1020) cfile6,isync,nsig,t2,ndf0,nfdot,csync, &
-                msg(1:28),msglen,nworst,navg,tana
+                msg(1:28),msglen,nworst,navg,tana,char(0)
         endif
      enddo
      if(last) exit
@@ -196,8 +196,8 @@ subroutine iscat(cdat0,npts0,nh,npct,t2,pick,cfile6,minsync,ntol,   &
   
   if(nlines.le.maxlines-1) nlines = nlines + 1
   write(line(nlines),1020) cfile6,isync,nsig,t2,ndf0,nfdot,csync,msg(1:28),  &
-       msglen,nworst,navg,tana
-1020 format(a6,2i4,f5.1,i5,i4,1x,a1,2x,a28,i4,2i3,f5.1)
+       msglen,nworst,navg,tana,char(0)
+1020 format(a6,2i4,f5.1,i5,i4,1x,a1,2x,a28,i4,2i3,f5.1,a1)
 
   return
 end subroutine iscat
