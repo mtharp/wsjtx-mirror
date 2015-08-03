f2ad3cd37d9b97a0737e237fb811cbc0ed62e87f
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2015-08-03 17:09:04 +0000

    Test program "fast9" now decodes a test file.
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx_exp@5748 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/fast9.f90
+++ lib/fast9.f90
@@ -3,19 +3,20 @@ program fast9
   parameter (NMAX=15*12000)
   parameter (NFFT=120,NH=NFFT/2,NQ=NFFT/4,JZ=NMAX/(NH/4))
   integer*2 id2(NMAX)
-
-  integer ii(16)                       !Locations of sync symbols
-  data ii/ 1,2,5,10,16,23,33,35,51,52,55,60,66,73,83,85/
   integer ii4(16)
-
+  integer*1 i1SoftSymbolsScrambled(207)
+  integer*1 i1SoftSymbols(207)
   real s1(JZ,NQ)
   real s2(340,NQ)
   real ss2(0:8,85)
+  real ss3(0:7,69)
   real s(NQ)
   real ccf(0:340-1,10)
   real x(NFFT)
   complex c(0:NH)
+  character*22 msg
   equivalence (x,c)
+  include 'jt9sync.f90'
 
 !  open(10,file='150730_191115.wav',access='stream',status='old')   !E
   open(10,file='150730_191345.wav',access='stream',status='old')   !H
@@ -23,8 +24,6 @@ program fast9
   npts=NMAX
   nsps=NH
   read(10) id2(1:npts)                   !Read the raw data
-
-  print*,NMAX,NFFT,NH,JZ,npts,nsps
 
   s=0
   s2=0
@@ -54,7 +53,7 @@ program fast9
   ii4=4*ii-3
   ccf=0.
   ccfbest=0.
-  do k=5,9
+  do k=1,10
      do lag=0,339
         t=0.
         do i=1,16
@@ -68,21 +67,29 @@ program fast9
            lagpk=lag
            kpk=k
         endif
-        if(k.eq.7) write(14,3002) lag,ccf(lag,7)
+        if(k.eq.7) write(14,3002) lag,ccf(lag,7)    !Blue
 3002    format(i6,f10.3)
      enddo
   enddo
 
+  do k=1,10
+     write(16,3002) k,ccf(lagpk,k)                  !Red
+  enddo
+
   ipk=7
-  print*,kpk,lagpk,ccfbest
 
   do i=0,8
      j4=lagpk-4
      i2=2*i + ipk
+     m=0
      do j=1,85
         j4=j4+4
         if(j4.gt.340) j4=j4-340
         ss2(i,j)=s2(j4,i2)
+        if(i.ge.1 .and. isync(j).eq.0) then
+           m=m+1
+           ss3(i-1,m)=ss2(i,j)
+        endif
      enddo
   enddo
 
@@ -91,5 +98,61 @@ program fast9
 3003 format(i2,9f8.2)
   enddo
 
-end program fast9
+! ###########################################
 
+  ss=0.
+  sig=0.
+  do j=1,69
+     smax=0.
+     do i=0,7
+        smax=max(smax,ss3(i,j))
+        ss=ss+ss3(i,j)
+     enddo
+     sig=sig+smax
+     ss=ss-smax
+  enddo
+  ave=ss/(69*7)                           !Baseline
+  call pctile(ss2,9*85,35,xmed)
+  ss3=ss3/ave
+  sig=sig/69.                             !Signal
+  t=max(1.0,sig - 1.0)
+  snrdb=db(t) - 61.3
+     
+  m0=3
+  k=0
+  scale=10.0
+
+  do j=1,69
+     do m=m0-1,0,-1                   !Get bit-wise soft symbols
+        if(m.eq.2) then
+           r1=max(ss3(4,j),ss3(5,j),ss3(6,j),ss3(7,j))
+           r0=max(ss3(0,j),ss3(1,j),ss3(2,j),ss3(3,j))
+        else if(m.eq.1) then
+           r1=max(ss3(2,j),ss3(3,j),ss3(4,j),ss3(5,j))
+           r0=max(ss3(0,j),ss3(1,j),ss3(6,j),ss3(7,j))
+        else
+           r1=max(ss3(1,j),ss3(2,j),ss3(4,j),ss3(7,j))
+           r0=max(ss3(0,j),ss3(3,j),ss3(5,j),ss3(6,j))
+        endif
+
+        k=k+1
+        i4=nint(scale*(r1-r0))
+        if(i4.lt.-127) i4=-127
+        if(i4.gt.127) i4=127
+        i1SoftSymbolsScrambled(k)=i4
+     enddo
+  enddo
+
+  limit=10000
+! Remove interleaving
+  call interleave9(i1SoftSymbolsScrambled,-1,i1SoftSymbols)
+
+  call jt9fano(i1SoftSymbols,limit,nlim,msg)
+
+  nsnr=nint(snrdb)
+  freq=ipk*df
+
+  write(*,1000) nutc,nsnr,xdt,nint(freq),msg
+1000 format(i4.4,i4,f5.1,i5,1x,'@',1x,a22)
+
+end program fast9
