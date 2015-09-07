14584b57b0e057b659ed6ca35f669bc90e1485a9
bsomervi <bsomervi@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2015-09-07 16:40:14 +0000

    Make Fortran hash function 64-bit safe
    This change foregos  greater than 32-bit integer length  strings to be
    hashed so that a default Fortran interface to the C nhash function can
    be offered.
    
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/branches/wsjtx_exp@5854 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- lib/hash.f90
+++ lib/hash.f90
@@ -4,8 +4,7 @@ subroutine hash(string,len,ihash)
   parameter (MASK15=32767)
 !  character*(*), target :: string
   character*1, target :: string
-  integer(c_size_t) len
-     i=nhash(c_loc(string),len,146)
+     i=nhash(c_loc(string),int(len,c_size_t),146)
      ihash=iand(i,MASK15)
   return
 end subroutine hash
