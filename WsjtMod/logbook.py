11120d663e8cdb8bcd6cd47c9ab76a7ed69568d6
ki7mt <ki7mt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2015-11-07 07:07:11 +0000

    WSJT - update logbook for g.modes to mode and tf to band conversions
    git-svn-id: svn://svn.code.sf.net/p/wsjt/wsjt/trunk@6057 ab8295b8-cf94-4d9e-aec4-7959e3be5d79

--- WsjtMod/logbook.py
+++ WsjtMod/logbook.py
@@ -27,128 +27,63 @@
 #-------------------------------------------------------------------------------
 
 import sys, os, csv, time
-import sqlite3
+from tkinter import *
+import tkinter as tk
+import sqlite3 as lite
 
-#--------------------------------------------------------------- submodes dict
+#--------------------------------------------------------------- submode dictionary
 '''
-    Sumbodes in dictionary form
+    Maps (g.modes) submode to ADIF v3.0.4 suported modes for logbook entry
 '''
 
-lb_submodes_dict = {"PCW": "CW",
-                    "CW": "CW",
-                    "FSK441": "FSK441",
-                    "ISCAT-A": "ISCAT",
-                    "ISCAT-B": "ISCAT",
-                    "JT6M": "JT6M",
-                    "JT4A": "JT4",
-                    "JT4B": "JT4",
-                    "JT4C": "JT4",
-                    "JT4D": "JT4",
-                    "JT4E": "JT4",
-                    "JT4F": "JT4",
-                    "JT4G": "JT4",
-                    "JT6M": "JT6M",
-                    "JT9-1": "JT9",
-                    "JT9-2": "JT9",
-                    "JT9-5": "JT9",
-                    "JT9-10": "JT9",
-                    "JT9-10": "JT9",
-                    "JT44": "JT44",
-                    "JT65A": "JT65",
-                    "JT65B": "JT65",
-                    "JT65B2": "JT65",
-                    "JT65C": "JT65",
-                    "JT65C2": "JT65",
-                    "WSPR-2": "WSPR",
-                    "WSPR-15": "WSPR",
+lb_submode={"PCW": "CW", \
+            "CW": "CW", \
+            "FSK441": "FSK441", \
+            "ISCAT-A": "ISCAT", \
+            "ISCAT-B": "ISCAT", \
+            "JT6M": "JT6M", \
+            "JT4A": "JT4", \
+            "JT4B": "JT4", \
+            "JT4C": "JT4", \
+            "JT4D": "JT4", \
+            "JT4E": "JT4", \
+            "JT4F": "JT4", \
+            "JT4G": "JT4", \
+            "JT6M": "JT6M", \
+            "JT9-1": "JT9", \
+            "JT9-2": "JT9", \
+            "JT9-5": "JT9", \
+            "JT9-10": "JT9", \
+            "JT9-10": "JT9", \
+            "JT44": "JT44", \
+            "JT65A": "JT65", \
+            "JT65B": "JT65", \
+            "JT65B2": "JT65", \
+            "JT65C": "JT65", \
+            "JT65C2": "JT65", \
+            "WSPR-2": "WSPR", \
+            "WSPR-15": "WSPR"
 }
 
-
-#--------------------------------------------------------------- submodes list
+#--------------------------------------------------------------- bands dictinary
 '''
-    Sumbodes in list form """
-'''
-
-lb_submodes_list = ('PCW',
-                    'CW',
-                    'ISCAT-A',
-                    'ISCAT-B',
-                    'JT6M',
-                    'JT44',
-                    'JT4A',
-                    'JT4B',
-                    'JT4C',
-                    'JT4D',
-                    'JT4E',
-                    'JT4F',
-                    'JT4G',
-                    'JT65A',
-                    'JT65B',
-                    'JT65B2',
-                    'JT65C',
-                    'JT65C2',
-                    'JT6M',
-                    'JT9-1',
-                    'JT9-10',
-                    'JT9-2',
-                    'JT9-30',
-                    'JT9-5',
-                    'FSK441',
-                    'WSPR-15',
-                    'WSPR-2'
-)
-
-
-#--------------------------------------------------------------- bands dict
-'''
-    Bands in dictionary form
+    Maps (tf) frequency to ADIF v3.0.4 suported bands for logbook entry
 '''
 
-lb_band_dict = {"2": "160m","4": "80m","7": "40m","10": "30m","14": "20m",\
-                "18": "17m","21": "15m","24": "12m","28": "12m","50": "2m",\
-                "70": "4m","144": "2m","222": "1.25m","432": "70cm",\
-                "902": "33cm","1296": "23cm","3456": "9cm","5760": "6cm",\
-                "10368": "3cm","24048": "1.25cm"
+lb_band={"2": "160m","4": "80m","7": "40m","10": "30m","14": "20m",\
+        "18": "17m","21": "15m","24": "12m","28": "12m","50": "2m",\
+        "70": "4m","144": "2m","222": "1.25m","432": "70cm",\
+        "902": "33cm","1296": "23cm","3456": "9cm","5760": "6cm",\
+        "10368": "3cm","24048": "1.25cm"
 }
-
-#--------------------------------------------------------------- bands list
-'''
-    Bands in list form
-'''
-
-lb_band_list=('160m',
-              '80m',
-              '40m',
-              '30m',
-              '20m',
-              '17m',
-              '15m',
-              '12m',
-              '10m',
-              '2m',
-              '4m',
-              '1.25m',
-              '70cm',
-              '33cm',
-              '23cm',
-              '9cm',
-              '6cm',
-              '3cm',
-              '1.25cm'
-)
-
-#------------------------------------------------------------------------------#
-#
-# Global Functions
-#
-#------------------------------------------------------------------------------#
-
 
 #--------------------------------------------------------------- cleab scren
-def clearscreen():
-    '''' * Simple clear screen function used for testing locally
-         * ALso set the GUI widget Icon file
-    '''
+'''' 
+    * Simple clear screen function used for testing locally
+    * Set the GUI widget Icon based on OS
+'''
+
+def clearScreen():
     if sys.platform == 'win32':
         os.system('cls')
         icon = ("wsjt.ico")
@@ -156,67 +91,61 @@ def clearscreen():
         os.system('clear')
         icon =  ("wsjt.jpg")
 
-
-#--------------------------------------------------------------- band 
-def lb_band_conv():
-    if tf == '2':
-        value='160m'
-    elif tf == '4':
-        value='80m'
-    elif tf == '7':
-        value='40m'
-    elif tf == '10':
-        value='30m'
-    elif tf == '14':
-        value='20m'
-    elif tf == '18':
-        value='17m'
-    elif tf == '21':
-        value='15m'
-    elif tf == '24':
-        value='12m'
-    elif tf == '28':
-        value='10m'
-    elif tf == '50':
-        band_entry='6m'
-    elif tf == '70':
-        band_entry='4m'
-    elif tf == '144':
-        band_entry='2m'
-    elif tf == '222':
-        band_entry='1.25m'
-    elif tf == '432':
-        band_entry='70cm'
-    elif tf == '902':
-        band_entry='33cm'
-    elif tf == '1296':
-        band_entry='23'
-    elif tf == '3456':
-        band_entry='9cm'
-    elif tf == '5760':
-        value == '6cm'
-    elif tf == '10368':
-        value='3cm'
-    elif tf == '24048':
-        value='1.25cm'
+#--------------------------------------------------------------- band convertion
+'''
+    Looks up the frequency supplied from (tf), then finds the associated
+    band (20m, 40m, 2m, etc) for logbook entry.
+    
+    Examples:
+              tf='14' or tf=str(g.nfreq)
+              logbook.bandConvert('14')
+              logbook.bandConvert(tf)
+'''
+def bandConvert(tf):
+    if tf in lb_band:
+        band=(str(lb_band[tf]))
+        print(tf, 'was converted to', band )
     else:
-        band_entry='' # create null value if nothing is passes from (tf)
+        print(tf, 'was not found in the conversion dictionary')
 
-#------------------------------------------------------------------------------#
-#
-# Test Database
-#
-#------------------------------------------------------------------------------#
+#--------------------------------------------------------------- submode converstion
+'''
+    Looks up the submode supplied from ( g.modes() ), then finds the associated
+    mode for logbook entry. e.g. JT65A --> JT65, ISCAT-B --> ISCAT
+    
+    Examples:
+              sm='JT65A' or str(g.mode)
+              logbook.submodeConvert('JT65A')
+              logbook.submodeConvert(sm)
+'''
+def submodeConvert(sm):
+    if sm in lb_submode:
+        mode=(str(lb_submode[sm]))
+        print(sm, 'was converted to', mode )
+    else:
+        print(sm, 'was not found in the conversion dictionary')
 
+
+#--------------------------------------------------------------- Test Database
 def testDatabase():
-    ''' test basic database functions '''
+    '''
+    Performs a series of simple test database activities
+        * Removes Old database ( test.db ) if exists
+        * Reads the sql input file ( logbook.sql )
+        * Creates all the database tables
+        * Populates the CALL3 table from ( c3csv.csv ) file
+        * Counts the number of tables ( nTables )
+        * Counts the number of records in ( logbook ) table
+        * Counts the number of records in ( call3 ) table
+        * Times the process from start to finish
+    '''
 
     dbf='test.db'
     sql_file='logbook.sql'
     c3csv='call3.csv'
 
     # clear and start the script
-    clearscreen()
+    clearScreen()
     query_time1 = time.time()
     print("----------------------------------------")
     print("GENERATE TEST DATABASE")
@@ -242,13 +171,14 @@ def testDatabase():
     cursor.executescript(script)
     fd.close()
     c3data = csv.reader(open(c3csv))
-    cursor.executemany('''INSERT into call3(id, call, gridsquare, force_init,
-                previous_call, comment, last_update)
-                values (?, ?, ?, ?, ?, ?, ?);''', c3data)
+    cursor.executemany('''INSERT into call3(id, call, gridsquare, force_init, 
+                            previous_call, comment, last_update) 
+                            values (?, ?, ?, ?, ?, ?, ?);''', c3data)
+
     print(" Commit changes ......: OK\n")
     db.commit()
 
-    # get SQL2 version
+    # get SQLite3 version
     print("DATABASE QUERIES")
     cursor.execute('SELECT SQLITE_VERSION()')
     Sv = cursor.fetchone()
@@ -277,9 +207,155 @@ def testDatabase():
     print(" Execution time.......: %.5f seconds\n" % query_time2)
 
 
+#--------------------------------------------------------------- Logbook Entry Form
+
+def entryForm():
+    form = tk.Tk()
+    form.title('WSJT Logbook')
+    form.resizable(0,0)
+
+    #------------------------------------------------------ Logbook UI Frames
+    # top frame (lbf1)
+    lbf1 = LabelFrame(form, text="  QSO Log Table ")
+    lbf1.grid(row=0, columnspan=7, sticky='W', padx=5, pady=5, ipadx=5, ipady=5)
+
+    # bottom left (lbf2)
+    lbf2 = LabelFrame(form, text="  Call3 Data Table")
+    lbf2.grid(row=2, columnspan=4, sticky='W', padx=5, pady=5, ipadx=8, ipady=8)
+
+    # bottom right (lbf2)
+    lbf3 = LabelFrame(form, text="  Save Options ")
+    lbf3.grid(row=2, column=6, columnspan=1, sticky='N', padx=4, pady=4, ipadx=0, ipady=0)
+
+    #------------------------------------------------------ QSO Log Frame
+    # Call Pmw.EntryField
+    lbf1_operator_Lbl = Label(lbf1, text="Callsign")
+    lbf1_operator_Lbl.grid(row=0, column=0, sticky='W', padx=5, pady=2)
+    lbf1_operator_txt = Entry(lbf1)
+    lbf1_operator_txt.insert(END, lb_operator)
+    lbf1_operator_txt.grid(row=1, column=0, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Date Pmw.EntryField w/Validator
+    lbf1_qso_date_lbl = Label(lbf1, text="Date (UTC)")
+    lbf1_qso_date_lbl.grid(row=0, column=1, sticky='W', padx=5, pady=2)
+    lbf1_qso_date_txt = Entry(lbf1)
+    lbf1_qso_date_txt.insert(END, lb_date)
+    lbf1_qso_date_txt.grid(row=1, column=1, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Time Pmw.EntryField w/Validator
+    lbf1_time_on_lbl= Label(lbf1, text="Time  (UTC)")
+    lbf1_time_on_lbl.grid(row=0, column=2, sticky='W', padx=5, pady=2)
+    lbf1_time_on_txt = Entry(lbf1)
+    lbf1_time_on_txt.insert(END, lb_time)
+    lbf1_time_on_txt.grid(row=1, column=2, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Submode Selection Pmw.Combobox
+    lbf1_submode_lbl = Label(lbf1, text="Mode Select")
+    lbf1_submode_lbl.grid(row=0, column=3, sticky='W', padx=5, pady=2)
+    lbf1_submode_txt = Entry(lbf1)
+    lbf1_submode_txt.insert(END, lb_submode)
+    lbf1_submode_txt.grid(row=1, column=3, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Band Slection Pmw.ComboBox
+    lbf1_band_lbl = Label(lbf1, text="Band Select")
+    lbf1_band_lbl.grid(row=0, column=4, sticky='W', padx=5, pady=2)
+    lbf1_band_txt = Entry(lbf1)
+    lbf1_band_txt.insert(END, lb_band)
+    lbf1_band_txt.grid(row=1, column=4, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Rpt_Sent Pmw.EntryField 
+    lbf1_rpt_sent_lbl = Label(lbf1, text="Rpt Sent")
+    lbf1_rpt_sent_lbl.grid(row=2, column=0, sticky='W', padx=5, pady=2)
+    lbf1_rpt_sent_txt = Entry(lbf1)
+    lbf1_rpt_sent_txt.grid(row=3, column=0, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Rpt_Rcvd
+    lbf1_rpt_rcvd_lbl = Label(lbf1, text="Rpt Rcvd")
+    lbf1_rpt_rcvd_lbl.grid(row=2, column=1, sticky='W', padx=5, pady=2)
+    lbf1_rpt_rcvd_txt = Entry(lbf1)
+    lbf1_rpt_rcvd_txt.insert(END, lb_rpt_rcvd)
+    lbf1_rpt_rcvd_txt.grid(row=3, column=1, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Grid
+    lbf1_gridsquare_lbl = Label(lbf1, text="Grid")
+    lbf1_gridsquare_lbl.grid(row=2, column=2, sticky='W', padx=5, pady=2)
+    lbf1_gridsquare_txt = Entry(lbf1)
+    lbf1_gridsquare_txt.insert(END, lb_gridsquare)
+    lbf1_gridsquare_txt.grid(row=3, column=2, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Name
+    lbf1_name_lbl = Label(lbf1, text="Name")
+    lbf1_name_lbl.grid(row=2, column=3, sticky='W', padx=5, pady=2)
+    lbf1_name_txt = Entry(lbf1)
+    lbf1_name_txt.grid(row=3, column=3, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # TxPwr
+    lbf1_tx_pwr_lbl = Label(lbf1, text="Tx Pwr")
+    lbf1_tx_pwr_lbl.grid(row=2, column=4, sticky='W', padx=5, pady=2)
+    lbf1_tx_pwr_txt = Entry(lbf1)
+    lbf1_tx_pwr_txt.grid(row=3, column=4, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # Comment
+    lbf1_comment_lbl= Label(lbf1, text="General Comments")
+    lbf1_comment_lbl.grid(row=7, column=0, sticky='W', padx=5, pady=2)
+    lbf1_comment_txt = Entry(lbf1)
+    lbf1_comment_txt.grid(row=8, column=0, columnspan=3, rowspan=2, padx=5, sticky="WE", pady=3)
+
+
+    #------------------------------------------------------ Call3 Data Table
+    # C3 Call
+    lbf2_operator_lbl = Label(lbf2, text="Callsign")
+    lbf2_operator_lbl.grid(row=0, column=0, sticky='W', padx=5, pady=2)
+    lbf2_operator_txt = Entry(lbf2)
+    lbf2_operator_txt.grid(row=1, column=0, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # C3 Grid
+    lbf2_gridsquare_lbl = Label(lbf2, text="Grid")
+    lbf2_gridsquare_lbl.grid(row=0, column=1, sticky='W', padx=5, pady=2)
+    lbf2_gridsquare_txt = Entry(lbf2)
+    lbf2_gridsquare_txt.grid(row=1, column=1, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # C3 notes
+    lbf2_notes_lbl= Label(lbf2, text="Previous Calls")
+    lbf2_notes_lbl.grid(row=0, column=2, sticky='W', padx=5, pady=2)
+    lbf2_notes_txt = Entry(lbf2)
+    lbf2_notes_txt.grid(row=1, column=2, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # C3 Last Update ( Non ADIF Spec Field )
+    lbf2_last_update_lbl = Label(lbf2, text="Last Update")
+    lbf2_last_update_lbl.grid(row=0, column=3, sticky='W', padx=5, pady=2)
+    lbf2_last_update_txt = Entry(lbf2)
+    lbf2_last_update_txt.grid(row=1, column=3, columnspan=1, padx=5, sticky="WE", pady=3)
+
+    # C3 Comment field
+    lbf2_comment_lbl = Label(lbf2, text="General Comments")
+    lbf2_comment_lbl.grid(row=3, column=0, sticky='W', padx=5, pady=2)
+    lbf2_comment_txt = Entry(lbf2)
+    lbf2_comment_txt.grid(row=4, column=0, columnspan=3, rowspan=2, padx=5, sticky="WE", pady=3)
+
+    #------------------------------------------------------ Save Options
+    # Update Call3 Data (checkbox)
+    lbf3_update_lbf2_txt = Checkbutton(lbf3, text="Update Call3 Table", onvalue=1, offvalue=0)
+    lbf3_update_lbf2_txt.grid(row=1, column=0, sticky='W', padx=5, pady=2)
+
+    # EME QSO Y/N (checkbox)
+    lbf3_is_eme_qso_txt = Checkbutton(lbf3, text="Contact is EME QSO", onvalue=1, offvalue=0)
+    lbf3_is_eme_qso_txt.grid(row=2, column=0, sticky='W', padx=5, pady=2)
+
+    # Save Buttons
+    lbf3_save_qso_button_txt = Button(lbf3, text=" Save ", fg="black", activebackground="green", background="green", command=form.destroy)
+    lbf3_save_qso_button_txt.grid(row=3, column=0, sticky='N', padx=1, pady=1, ipadx=1, ipady=1)
+
+    # Cancel Button
+    lbf3_cancel_button_txt = Button(lbf3, text="Cancel", fg="black", activebackground="red", background="red", command=form.destroy)
+    lbf3_cancel_button_txt.grid(row=4, column=0, sticky='S', padx=1, pady=1, ipadx=1, ipady=1)
+
+    form.mainloop()
+
 #------------------------------------------------------------------------------#
 
 if __name__ == '__main__':
-    testDatabase()
+    testDatabase()         # run basic tests
+    # entryForm()          # test the  mogbook form
 
-# END WSJTDB MODULE
+# END LOGBOOK MODULE
